109368c03f3c145b070028b59f104c282db006a4
Dave Halter <davidhalter88@gmail.com>
2020-10-25 15:44:19 +0100

    Add a basic definition of nodes

--- parsa/src/lib.rs
+++ parsa/src/lib.rs
@@ -1,7 +1,52 @@
+#![allow(unused)]
+
+struct Tree {
+    code: Box<str>,
+    nodes: Vec<Node>,
+}
+
+
+#[derive(Copy, Clone)]
+struct Node {
+    next_node_offset: u32,
+    // Positive values are token types, negative values are nodes
+    node_type: i16,
+
+    start_index: u32,
+    length: u16,
+    extra_data: u32,
+}
+
+impl Node {
+    pub fn get_extra_data(&self) -> u32 {
+        self.extra_data
+    }
+
+    pub fn set_extra_data(&mut self, extra_data: u32) {
+        self.extra_data = extra_data
+    }
+}
+
+struct CompressedNode {
+    next_node_offset: u8,
+    // Positive values are token types, negative values are nodes
+    node_type: i8,
+
+    start_index: u16,
+    length: u16,
+    extra_data1: u16,
+    extra_data2: u16,
+}
+
+
 #[cfg(test)]
 mod tests {
+    use std::mem::{size_of, align_of};
+    use super::*;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn sizes() {
+        assert_eq!(size_of::<Node>(), 16);
+        assert_eq!(size_of::<CompressedNode>(), 10);
     }
 }
