aaed2e9c04cbaf21c4cbf323c3582de1249ad15e
Dave Halter <davidhalter88@gmail.com>
2020-11-07 15:48:02 +0100

    Move a lot of code around for automatons

--- parsa/src/grammar.rs
+++ parsa/src/grammar.rs
@@ -1,14 +1,44 @@
-use std::cell::RefCell;
-use std::rc::{Rc, Weak};
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use crate::{InternalType, Rule};
 
-type NFAStateRc = Rc<RefCell<NFAState>>;
+type NFAStateId = usize;
 
 #[derive(Default)]
 struct RuleAutomaton {
-    nfa_states: Vec<NFAStateRc>,
+    nfa_states: Vec<NFAState>,
 }
+
+impl RuleAutomaton {
+    fn get_nfa_state(&mut self, id: NFAStateId) -> &mut NFAState {
+        &mut self.nfa_states[id]
+    }
+
+    fn new_nfa_states(&mut self) -> (NFAStateId, NFAStateId) {
+        let mut new = || {
+            self.nfa_states.push(Default::default());
+            self.nfa_states.len() - 1
+        };
+        (new(), new())
+    }
+
+    fn add_transition(&mut self, start: NFAStateId, to: NFAStateId, transition: NFATransitionType) {
+        self.get_nfa_state(start).transitions.push(
+            NFATransition {transition: transition, to: to}
+        );
+    }
+
+    fn add_empty_transition(&mut self, start: NFAStateId, to: NFAStateId) {
+        self.add_transition(start, to, NFATransitionType::None);
+    }
+
+    fn group_nfas(nfa_state: NFAStateId) -> HashSet<NFAStateId> {
+        // Group all NFAs that are ε-moves
+        let mut set = HashSet::new();
+        set.insert(nfa_state);
+        set
+    }
+}
+
 
 // NFA = nondeterministic finite automaton
 #[derive(Default)]
@@ -25,7 +55,7 @@ enum NFATransitionType {
 
 struct NFATransition {
     transition: NFATransitionType,
-    to: Weak<RefCell<NFAState>>,
+    to: NFAStateId,
 }
 
 pub trait Grammar {
@@ -35,6 +65,7 @@ pub trait Grammar {
         for (internal_type, rule) in rules {
             let mut automaton = Default::default();
             let (start, end) = build_automaton(self, &mut automaton, rule);
+            nfa_to_dfa(start, end);
             dbg!(rule);
         }
     }
@@ -43,58 +74,42 @@ pub trait Grammar {
     }
 }
 
-fn new_states(automaton: &mut RuleAutomaton) -> (NFAStateRc, NFAStateRc) {
-    automaton.nfa_states.push(Default::default());
-    automaton.nfa_states.push(Default::default());
-    (Rc::clone(&automaton.nfa_states[0]), Rc::clone(&automaton.nfa_states[0]))
-}
-
-fn add_transition(start: &NFAStateRc, to: &NFAStateRc, transition: NFATransitionType) {
-    start.borrow_mut().transitions.push(
-        NFATransition {transition: transition, to: Rc::downgrade(to)}
-    );
-}
-
-fn add_empty_transition(start: &NFAStateRc, to: &NFAStateRc) {
-    add_transition(start, to, NFATransitionType::None);
-}
-
-fn build_automaton(grammar: &dyn Grammar, automaton: &mut RuleAutomaton, rule: &Rule) -> (NFAStateRc, NFAStateRc) {
+fn build_automaton(grammar: &dyn Grammar, automaton: &mut RuleAutomaton, rule: &Rule) -> (NFAStateId, NFAStateId) {
     use Rule::*;
     match *rule {
         Identifier(string) => {
-            let (start, end) = new_states(automaton);
+            let (start, end) = automaton.new_nfa_states();
             if let Some(t) = grammar.terminal_name_to_int(string) {
-                add_transition(&start, &end, NFATransitionType::Terminal(t));
+                automaton.add_transition(start, end, NFATransitionType::Terminal(t));
             } else if let Some(t) = grammar.nonterminal_name_to_int(string) {
-                add_transition(&start, &end, NFATransitionType::Nonterminal(t));
+                automaton.add_transition(start, end, NFATransitionType::Nonterminal(t));
             } else {
                 panic!("No terminal / nonterminal found for {}", string);
             }
             (start, end)
         },
         Keyword(string) => {
-            let (start, end) = new_states(automaton);
-            add_transition(&start, &end, NFATransitionType::Keyword(string));
+            let (start, end) = automaton.new_nfa_states();
+            automaton.add_transition(start, end, NFATransitionType::Keyword(string));
             (start, end)
         },
         Or(rule1, rule2) => {
-            let (start, end) = new_states(automaton);
+            let (start, end) = automaton.new_nfa_states();
             for r in [rule1, rule2].iter() {
                 let (x, y) = build_automaton(grammar, automaton, r);
-                add_empty_transition(&start, &x);
-                add_empty_transition(&y, &end);
+                automaton.add_empty_transition(start, x);
+                automaton.add_empty_transition(y, end);
             }
             (start, end)
         },
         Maybe(rule1) => {
             let (start, end) = build_automaton(grammar, automaton, rule1);
-            add_empty_transition(&start, &end);
+            automaton.add_empty_transition(start, end);
             (start, end)
         },
         Multiple(rule1) => {
             let (start, end) = build_automaton(grammar, automaton, rule1);
-            add_empty_transition(&end, &start);
+            automaton.add_empty_transition(end, start);
             (start, end)
         }
         NegativeLookahead(rule1) => {
@@ -109,7 +124,7 @@ fn build_automaton(grammar: &dyn Grammar, automaton: &mut RuleAutomaton, rule: &
         Cut(rule1, rule2) | Next(rule1, rule2) => {
             let (start1, end1) = build_automaton(grammar, automaton, rule1);
             let (start2, end2) = build_automaton(grammar, automaton, rule2);
-            add_empty_transition(&end1, &start1);
+            automaton.add_empty_transition(end1, start1);
             (start1, end2)
         }
     }
@@ -118,4 +133,9 @@ fn build_automaton(grammar: &dyn Grammar, automaton: &mut RuleAutomaton, rule: &
 struct DFAState {
     transitions: Vec<u8>,
     is_final: bool,
+}
+
+fn nfa_to_dfa(start: NFAStateId, end: NFAStateId) -> u8 {
+    //let dfa_states = vec!();
+    1
 }
