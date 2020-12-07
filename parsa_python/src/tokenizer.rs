688c63aa38b6730be6b0e3531dcc2f2773e8b66d
Dave Halter <davidhalter88@gmail.com>
2020-12-08 00:31:43 +0100

    Write a lot more of the tokenizer

--- parsa_python/src/tokenizer.rs
+++ parsa_python/src/tokenizer.rs
@@ -14,9 +14,10 @@ const BIN_NUMBER: &str = r"0[bB](_?[01])+";
 const OCT_NUMBER: &str = r"0[oO](_?[0-7])+";
 const DEC_NUMBER: &str = r"(0(_?0)*|[1-9](_?[0-9])*)";
 const EXPONENT: &str = r"[eE][-+]?[0-9](?:_?[0-9])*";
+const BASIC_WHITESPACE: &str = r"^[\f\t ]*#[^\r\n]*";
 
 lazy_static::lazy_static! {
-    static ref WHITESPACE: Regex = r(r"^[\f\t ]*#[^\r\n]*");
+    static ref WHITESPACE: Regex = r(&format!(r"{}(\\{})*", BASIC_WHITESPACE, BASIC_WHITESPACE));
 
     static ref INT_NUMBER: String = or(&[HEX_NUMBER, BIN_NUMBER, OCT_NUMBER, DEC_NUMBER]);
     static ref EXP_FLOAT: String = or(&[r"[0-9](_?[0-9])*", EXPONENT]);
@@ -84,17 +85,12 @@ create_token!(struct PythonToken, enum PythonTokenType,
 pub struct PythonTokenizer<'a> {
     code: &'a str,
 
+    previous_token_was_newline: bool,
     index: usize,
     ended: bool,
-    indents: Vec<Indent>,
+    indent_stack: Vec<usize>,
     parentheses_level: usize,
     f_string_stack: Vec<FStringNode>,
-}
-
-#[derive(Debug)]
-struct Indent {
-    start: CodeIndex,
-    indentation_count: usize,
 }
 
 #[derive(Debug)]
@@ -170,38 +166,36 @@ impl PythonTokenizer<'_> {
 
     #[inline]
     fn dedent_if_necessary(&mut self, indentation_count: usize) -> Option<PythonToken> {
-        while indentation_count < self.indents.last().unwrap().indentation_count {
-            if indentation_count > self.indents[self.indents.len() - 2].indentation_count {
-                self.indents.last_mut().unwrap().indentation_count = indentation_count;
+        while indentation_count < *self.indent_stack.last().unwrap() {
+            if indentation_count > self.indent_stack[self.indent_stack.len() - 2] {
+                *self.indent_stack.last_mut().unwrap() = indentation_count;
                 return self.new_tok(self.index, false, PythonTokenType::ErrorDedent);//, (lnum, start));
             }
-            self.indents.pop();
+            self.indent_stack.pop();
             return self.new_tok(self.index, false, PythonTokenType::Dedent);//, spos);
         }
         None
     }
 
     #[inline]
-    fn code_from_start(&self) -> &str {
-        // This function only exists, because the regex crate is not able to at
-        // the beginning of a specific position.
-        unsafe {
-            str::from_utf8_unchecked(&self.code.as_bytes()[self.index as usize..])
+    fn encountered_break_token(&mut self) -> Option<PythonToken> {
+        if self.parentheses_level != 0 || self.f_string_stack.len() != 0 {
+            self.parentheses_level = 0;
+            self.f_string_stack.clear();
+
+            let mut indentation = 0;
+            for character in self.code[..self.index].chars().rev() {
+                if character == ' ' || character == '\t' {
+                    indentation += 1;
+                } else if character == '\n' || character == '\r' {
+                    // We only want to dedent if the token is on a new line.
+                    return self.dedent_if_necessary(indentation)
+                } else {
+                    return None;
+                }
+            }
         }
-    }
-
-    #[inline]
-    fn encountered_break_token(&mut self) {
-        self.parentheses_level = 0;
-        self.f_string_stack.clear();
-
-        /*
-        // We only want to dedent if the token is on a new line.
-        m = re.match(r'[ \f\t]*$', line[:start])
-        if m is not None:
-            yield from dedent_if_necessary(m.end())
-
-            */
+        None
     }
 }
 
@@ -212,18 +206,38 @@ impl Iterator for PythonTokenizer<'_> {
             return None;
         }
 
-
         if let Some(match_) = WHITESPACE.find(code_from_start(self.code, self.index)) {
             self.index += match_.end();
         }
 
         let start = self.index;
         let c = code_from_start(self.code, self.index);
-        if let Some(match_) = OPERATOR.find(c) {
-            // TODO ; always break
-            self.index = match_.end();
+        if self.previous_token_was_newline {
+            self.previous_token_was_newline = false;
+            if let Some(&character) = c.as_bytes().first() {
+                if character != b'\n' && character != b'\r' {
+                    if self.parentheses_level == 0 && self.f_string_stack.len() == 0 {
+                        let indentation_count = start;
+                        if indentation_count > *self.indent_stack.last().unwrap() {
+                            self.indent_stack.push(indentation_count);
+                            return self.new_tok(start, false, PythonTokenType::Indent);
+                        } else {
+                            if let Some(token) = self.dedent_if_necessary(start - self.index) {
+                                return Some(token);
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
+        if let Some(match_) = OPERATOR.find(c) {
             let character = c.as_bytes()[0];
+            if character == b';' {
+                self.encountered_break_token();
+            }
+            self.index += match_.end();
+
             if character == b'(' || character == b'[' || character == b'{' {
                 match self.f_string_stack.last_mut() {
                     None => self.parentheses_level += 1,
@@ -245,8 +259,6 @@ impl Iterator for PythonTokenizer<'_> {
                         }
                     },
                 }
-            } else if character == b';' {
-                self.encountered_break_token();
             }
             return self.new_tok(start, true, PythonTokenType::Operator);
         }
@@ -257,39 +269,12 @@ impl Iterator for PythonTokenizer<'_> {
         ];
         for (r, token_type) in &regexes {
             if let Some(match_) = r.find(c) {
-                self.index = match_.end();
+                self.index += match_.end();
                 return self.new_tok(start, false, *token_type);
             }
         }
-        if let Some(match_) = NAME.find(c) {
-            if ALWAYS_BREAK_NAMES.contains(match_.as_str()) {
-                self.encountered_break_token();
-
-            }
-            self.index = match_.end();
-            /*
-            if token.isidentifier():
-                yield PythonToken(NAME, token, spos, prefix)
-            else:
-                yield from _split_illegal_unicode_name(token, spos, prefix)
-            */
-            return self.new_tok(start, true, PythonTokenType::Name);
-        }
-        if let Some(match_) = NEWLINE.find(c) {
-            /*
-            if any(not f.allow_multiline() for f in fstring_stack):
-                fstring_stack.clear()
-            */
-
-            self.index = match_.end();
-            if self.parentheses_level == 0 && self.f_string_stack.len() == 0 {
-                return self.new_tok(start, false, PythonTokenType::Newline);
-            } else {
-                return self.next();
-            }
-        }
         if let Some(match_) = F_STRING_START.find(c) {
-            self.index = match_.end();
+            self.index += match_.end();
             self.f_string_stack.push(FStringNode {
                 quote: {
                     let string = match_.as_str();
@@ -309,17 +294,56 @@ impl Iterator for PythonTokenizer<'_> {
             return self.new_tok(start, false, PythonTokenType::F_StringStart);
         }
 
+        if let Some(match_) = NAME.find(c) {
+            if ALWAYS_BREAK_NAMES.contains(match_.as_str()) {
+                self.encountered_break_token();
+
+            }
+            self.index += match_.end();
+            /*
+            if token.isidentifier():
+                yield PythonToken(NAME, token, spos, prefix)
+            else:
+                yield from _split_illegal_unicode_name(token, spos, prefix)
+            */
+            return self.new_tok(start, true, PythonTokenType::Name);
+        }
+
+        if let Some(match_) = NEWLINE.find(c) {
+            if self.f_string_stack.len() != 0 {
+                match self.f_string_stack.iter().enumerate().find(
+                        |(i, node)| !node.allow_multiline()) {
+                    None => {},
+                    Some((i, _)) => {
+                        self.f_string_stack.drain(i..);
+                    }
+                }
+            }
+
+            self.index += match_.end();
+            if self.parentheses_level == 0 && self.f_string_stack.len() == 0 {
+                self.previous_token_was_newline = true;
+                return self.new_tok(start, false, PythonTokenType::Newline);
+            } else {
+                return self.next();
+            }
+        }
+
         match c.as_bytes().first() {
             None => {
-                self.ended = true;
-                self.new_tok(start, false, PythonTokenType::Endmarker)
+                if self.indent_stack.len() != 0 {
+                    self.indent_stack.pop();
+                    self.new_tok(start, false, PythonTokenType::Dedent)
+                } else {
+                    self.ended = true;
+                    self.new_tok(start, false, PythonTokenType::Endmarker)
+                }
             },
             Some(_) => {
                 self.index += 1;
                 self.new_tok(start, false, PythonTokenType::ErrorToken)
             },
         }
-
     }
 
 }
