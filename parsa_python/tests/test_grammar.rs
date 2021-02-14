aa2e59cd3ef263a86441c28bd22d26d60aa01e3a
Dave Halter <davidhalter88@gmail.com>
2021-02-14 14:39:48 +0100

    Make sure that all Python code is also valid in the 3.10 grammar

--- parsa_python/tests/test_grammar.rs
+++ parsa_python/tests/test_grammar.rs
@@ -75,7 +75,7 @@ parametrize_snapshots!(
         ");
     calls: &dedent("
         foo(bar)
-        foo(bar.foo=3)
+        foo(bar_foo=3)
         foo(bar.baz:=2)
         foo(**kwargs)
         ");
