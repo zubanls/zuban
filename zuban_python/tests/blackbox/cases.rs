99f4acf4cc502c0cf3723b9039d43cfc403be03b
Dave Halter <davidhalter88@gmail.com>
2021-06-23 01:47:30 +0200

    Add a way for blackbox testing

---
+++ zuban_python/tests/blackbox/cases.rs
@@ -0,0 +1,74 @@
+use std::path::PathBuf;
+use std::collections::HashSet;
+use zuban_python::{Script, Position};
+
+pub struct TestFile {
+    pub path: PathBuf,
+    pub code: String,
+}
+
+#[derive(Debug)]
+struct TestCase {
+    line: usize,
+    column: usize,
+    type_: CaseType,
+}
+
+#[derive(Debug)]
+enum CaseType {
+    Infer(HashSet<String>),
+}
+
+impl TestFile {
+    pub fn test(&self) {
+        let mut project = zuban_python::Project::new("foo".to_owned());
+        let script = zuban_python::Script::new(
+            &mut project,
+            Some(self.path.to_str().unwrap().to_owned()),
+            Some(self.code.clone()),
+        );
+        for case in self.get_test_cases() {
+            match case.type_ {
+                CaseType::Infer(expected) => {
+                    let actual: HashSet<_> = script
+                        .infer_definition(Position::LineColumn(case.line, case.column))
+                        .iter()
+                        .map(|name| name.get_name().to_owned())
+                        .collect();
+                    assert_eq!(actual, expected);
+                }
+            }
+        }
+    }
+
+    fn get_test_cases(&self) -> Vec<TestCase> {
+        let mut cases = vec![];
+        let lines: Vec<_> = self.code.split('\n').collect();
+        for (i, line) in lines.iter().enumerate() {
+            let trimmed = line.trim_start();
+            if trimmed.starts_with("#? ") {
+                let mut names: Vec<_> = trimmed[3..].split(" ").collect();
+                let column = {
+                    if let Ok(c) = names[0].parse() {
+                        names.remove(0);
+                        c
+                    } else {
+                        lines[i + 1].len()
+                    }
+                };
+                cases.push(TestCase {
+                    line: i + 1,
+                    column: 1,
+                    type_: CaseType::Infer(names.iter().cloned().map(|x| x.to_owned()).collect()),
+                });
+            }
+        }
+        cases
+    }
+}
+
+impl std::fmt::Debug for TestFile {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        f.debug_struct("TestFile").field("path", &self.path).finish()
+    }
+}
---
+++ zuban_python/tests/blackbox/main.rs
@@ -0,0 +1,27 @@
+mod cases;
+
+use std::env;
+use std::fs::{read_dir, read_to_string};
+use std::path::PathBuf;
+
+fn main() {
+    let cli_args: Vec<String> = env::args().collect();
+
+    for python_file in get_python_files() {
+        let code = read_to_string(&python_file).unwrap();
+        cases::TestFile {path: python_file, code}.test();
+    }
+}
+
+fn get_python_files() -> Vec<PathBuf> {
+    let mut p = PathBuf::from(file!().replace("zuban_python/", ""));
+    assert!(p.pop());
+    p.push("python_files");
+
+    let mut entries: Vec<_> = read_dir(p)
+        .unwrap()
+        .map(|res| res.map(|e| e.path()).unwrap())
+        .collect();
+    entries.sort();
+    entries
+}
