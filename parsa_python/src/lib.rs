b704452d4b42b382d7b9f06971782fa46be95f89
Dave Halter <davidhalter88@gmail.com>
2021-08-15 00:03:15 +0200

    Introduce function_def_parameters

--- parsa_python/src/lib.rs
+++ parsa_python/src/lib.rs
@@ -117,7 +117,8 @@ create_grammar!(
     keyword_pattern: Name "=" pattern
 
     async_function_def: "async" function_def
-    function_def: "def" name_definition "(" [parameters] ")" ["->" expression] ":" block
+    function_def: "def" name_definition function_def_parameters ["->" expression] ":" block
+    function_def_parameters: "(" [parameters] ")"
 
     decorator: "@" named_expression Newline
     decorators: decorator+
