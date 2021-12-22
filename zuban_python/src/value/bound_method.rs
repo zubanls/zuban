e3097125eda06ebc43401686c5bf8c4d391e60f9
Dave Halter <davidhalter88@gmail.com>
2021-12-22 02:11:04 +0100

    Reenable bound methods

--- zuban_python/src/value/bound_method.rs
+++ zuban_python/src/value/bound_method.rs
@@ -36,14 +36,14 @@ impl<'db, 'a, 'b> Value<'db, 'b> for BoundMethod<'db, 'a> {
     ) -> Inferred<'db> {
         let inferred = Inferred::from_any_link(i_s.database, self.instance);
         // TODO wtf why is this annotation necessary
-        /*
-        let func: &dyn Value<'_, '_> = self.function;
+        // TODO executing func.execute outside of the closure works, weird...
+        // https://github.com/rust-lang/rust/issues/91942
         inferred.run_on_value(i_s, &mut |i_s, value| {
+            let value: &dyn Value = unsafe { std::mem::transmute(value) };
             let instance = value.as_instance().unwrap();
             let args = InstanceArguments::new(instance, args);
+            let func: &dyn Value = unsafe { std::mem::transmute(self.function) };
             func.execute(i_s, &args)
         })
-        */
-        todo!()
     }
 }
