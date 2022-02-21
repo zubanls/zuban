99d05d8e33d2eef4930a346c9e45c3d43638a528
Dave Halter <davidhalter88@gmail.com>
2022-02-21 22:57:18 +0100

    Remove primary_index from slice details

--- zuban_python/src/getitem.rs
+++ zuban_python/src/getitem.rs
@@ -49,17 +49,14 @@ impl<'db> SliceType<'db> {
         match self.ast_node {
             ASTSliceType::NamedExpression(named_expr) => SliceTypeContent::Simple(Simple {
                 file: self.file,
-                primary_index: self.primary_index,
                 named_expr,
             }),
             ASTSliceType::Slice(slice) => SliceTypeContent::Slice(Slice {
                 file: self.file,
-                primary_index: self.primary_index,
                 slice,
             }),
             ASTSliceType::Slices(slices) => SliceTypeContent::Slices(Slices {
                 file: self.file,
-                primary_index: self.primary_index,
                 slices,
             }),
         }
@@ -69,7 +66,6 @@ impl<'db> SliceType<'db> {
 #[derive(Debug, Copy, Clone)]
 pub struct Simple<'db> {
     pub file: &'db PythonFile,
-    pub primary_index: NodeIndex,
     pub named_expr: NamedExpression<'db>,
 }
 
@@ -90,20 +86,18 @@ impl<'db> Simple<'db> {
 #[derive(Debug, Copy, Clone)]
 pub struct Slice<'db> {
     file: &'db PythonFile,
-    primary_index: NodeIndex,
     slice: ASTSlice<'db>,
 }
 
 #[derive(Debug, Copy, Clone)]
 pub struct Slices<'db> {
     pub file: &'db PythonFile,
-    pub primary_index: NodeIndex,
     slices: ASTSlices<'db>,
 }
 
 impl<'db> Slices<'db> {
     pub fn iter(&self) -> SliceIterator<'db> {
-        SliceIterator(self.file, self.primary_index, self.slices.iter())
+        SliceIterator(self.file, self.slices.iter())
     }
 }
 
@@ -121,22 +115,20 @@ impl<'db> SliceOrSimple<'db> {
     }
 }
 
-pub struct SliceIterator<'db>(&'db PythonFile, NodeIndex, ASTSliceIterator<'db>);
+pub struct SliceIterator<'db>(&'db PythonFile, ASTSliceIterator<'db>);
 
 impl<'db> Iterator for SliceIterator<'db> {
     type Item = SliceOrSimple<'db>;
 
     fn next(&mut self) -> Option<Self::Item> {
         // TODO it's actually a bad idea to pass primary_index here
-        self.2.next().map(|content| match content {
+        self.1.next().map(|content| match content {
             SliceContent::NamedExpression(n) => SliceOrSimple::Simple(Simple {
                 file: self.0,
-                primary_index: self.1,
                 named_expr: n,
             }),
             SliceContent::Slice(s) => SliceOrSimple::Slice(Slice {
                 file: self.0,
-                primary_index: self.1,
                 slice: s,
             }),
         })
