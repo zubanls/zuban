5d6efdb617963c15456c8add87f6b9620e24f38c
Dave Halter <davidhalter88@gmail.com>
2022-03-18 12:34:52 +0100

    Fix inference of classes with type vars. YEEEES

--- zuban_python/src/file/mod.rs
+++ zuban_python/src/file/mod.rs
@@ -25,7 +25,7 @@ use crate::name::{Names, TreeName, TreePosition};
 use crate::name_binder::{NameBinder, NameBinderType};
 use crate::node_ref::NodeRef;
 use crate::utils::{debug_indent, InsertOnlyVec, SymbolTable};
-use crate::value::{Function, Module, Value};
+use crate::value::{ClassLike, Function, Module, Value};
 use crate::workspaces::DirContent;
 
 #[derive(Default, Debug)]
@@ -804,59 +804,46 @@ impl<'db, 'a, 'b> PythonInference<'db, 'a, 'b> {
         let mut inference = self.file.inference(&mut i_s);
         let inferred = inference.infer_annotation_expression_class(expr);
         // TODO locality is wrong!!!!!1
-        let point = if inferred
-            .maybe_simple(inference.i_s, |v| v.as_class().cloned())
-            .is_some()
-        {
+        let type_ = inferred.as_type(self.i_s);
+        let point = if matches!(type_, Type::Unknown) {
+            let node_ref = NodeRef::new(self.file, expr.index());
+            if let Some(func) = inferred.maybe_simple(inference.i_s, |v| v.as_function().cloned()) {
+                node_ref.add_typing_issue(
+                    i_s.database,
+                    IssueType::ValidType(format!(
+                        "Function {:?} is not valid as a type",
+                        func.qualified_name(i_s.database),
+                    )),
+                );
+                node_ref.add_typing_issue(
+                    i_s.database,
+                    IssueType::Note(
+                        "Perhaps you need \"Callable[...]\" or a callback protocol?".to_owned(),
+                    ),
+                )
+            } else if let Some(module) =
+                inferred.maybe_simple(inference.i_s, |v| v.as_module().cloned())
+            {
+                node_ref.add_typing_issue(
+                    i_s.database,
+                    IssueType::ValidType(format!(
+                        "Module {:?} is not valid as a type",
+                        module.qualified_name(i_s.database),
+                    )),
+                );
+            } else {
+                debug!("Unknown annotation expression {}", expr.short_debug());
+            }
+            Point::new_unknown(self.file.file_index(), Locality::Todo)
+        } else if type_.has_type_vars(self.i_s) {
+            // Cannot save this, because different type vars change the output.
+            return type_.execute_and_resolve_type_vars(self.i_s, self.i_s.current_class, None);
+        } else if matches!(type_, Type::ClassLike(ClassLike::Class(_))) {
             Point::new_simple_specific(Specific::AnnotationInstance, Locality::Todo)
         } else {
-            let type_ = inferred.as_type(self.i_s);
-            if matches!(type_, Type::Unknown) {
-                let node_ref = NodeRef::new(self.file, expr.index());
-                if let Some(func) =
-                    inferred.maybe_simple(inference.i_s, |v| v.as_function().cloned())
-                {
-                    node_ref.add_typing_issue(
-                        i_s.database,
-                        IssueType::ValidType(format!(
-                            "Function {:?} is not valid as a type",
-                            func.qualified_name(i_s.database),
-                        )),
-                    );
-                    node_ref.add_typing_issue(
-                        i_s.database,
-                        IssueType::Note(
-                            "Perhaps you need \"Callable[...]\" or a callback protocol?".to_owned(),
-                        ),
-                    )
-                } else if let Some(module) =
-                    inferred.maybe_simple(inference.i_s, |v| v.as_module().cloned())
-                {
-                    node_ref.add_typing_issue(
-                        i_s.database,
-                        IssueType::ValidType(format!(
-                            "Module {:?} is not valid as a type",
-                            module.qualified_name(i_s.database),
-                        )),
-                    );
-                } else {
-                    debug!("Unknown annotation expression {}", expr.short_debug());
-                }
-                Point::new_unknown(self.file.file_index(), Locality::Todo)
-            } else {
-                if type_.has_type_vars(self.i_s) {
-                    // Cannot save this, because different type vars change the output.
-                    return type_.execute_and_resolve_type_vars(
-                        self.i_s,
-                        self.i_s.current_class,
-                        None,
-                    );
-                } else {
-                    return type_
-                        .maybe_execute(self.i_s)
-                        .save_redirect(self.file, annotation_index);
-                }
-            }
+            return type_
+                .maybe_execute(self.i_s)
+                .save_redirect(self.file, annotation_index);
         };
         Inferred::new_and_save(self.file, annotation_index, point)
     }
