0d8b8ca7b8b5ad3d6bab85c6cf3399d791bbcd14
Dave Halter <davidhalter88@gmail.com>
2022-06-21 00:34:04 +0200

    Enable check-basic.test

--- zuban_python/tests/mypylike/main.rs
+++ zuban_python/tests/mypylike/main.rs
@@ -6,15 +6,17 @@ use std::time::Instant;
 
 use regex::{Captures, Regex, Replacer};
 
-const USE_MYPY_TEST_FILES: [&str; 6] = [
+const USE_MYPY_TEST_FILES: [&str; 7] = [
     "fine-grained.test",
     "check-generics.test",
     "check-generic-alias.test",
     "semanal-errors.test",
     "check-semanal-error.test",
     "check-newsemanal.test",
-    //"check-basic.test",
+    "check-basic.test",
     //"check-type-aliases.test",
+    //"check-typevar-values.test",
+    //"check-bound.test",
     //"check-modules.test",
     //"check-modules-case.test",
     //"check-functions.test",
@@ -30,8 +32,6 @@ const USE_MYPY_TEST_FILES: [&str; 6] = [
     //"check-unions.test",
     //"check-union-or-syntax.test",
     //"check-protocols.test",
-    //"check-typevar-values.test",
-    //"check-bound.test",
     //"check-generic-subtyping.test",
     //"check-callable.test",
     //"check-incremental.test",
