cb52dd13d8391de49eb8c493763126d1027c6a0c
Dave Halter <davidhalter88@gmail.com>
2022-07-23 03:09:43 +0200

    Forgot to ad the never file

---
+++ zuban_python/src/value/never.rs
@@ -0,0 +1,37 @@
+use super::{ClassLike, LookupResult, Value, ValueKind};
+use crate::debug;
+use crate::getitem::SliceType;
+use crate::inference_state::InferenceState;
+use crate::inferred::Inferred;
+
+#[derive(Debug)]
+pub struct NeverInstance();
+
+impl<'db, 'a> Value<'db, 'a> for NeverInstance {
+    fn kind(&self) -> ValueKind {
+        ValueKind::Constant
+    }
+
+    fn name(&self) -> &'db str {
+        "<nothing>"
+    }
+
+    fn lookup_internal(&self, i_s: &mut InferenceState<'db, '_>, name: &str) -> LookupResult<'db> {
+        debug!("TODO None lookup");
+        todo!("TODO report error for never.{name}");
+        //LookupResult::None
+    }
+
+    fn class(&self, i_s: &mut InferenceState<'db, '_>) -> ClassLike<'db, 'a> {
+        ClassLike::Never
+    }
+
+    fn get_item(
+        &self,
+        i_s: &mut InferenceState<'db, '_>,
+        slice_type: &SliceType<'db, '_>,
+    ) -> Inferred<'db> {
+        todo!("TODO report error for never[...]");
+        //Inferred::new_any()
+    }
+}
