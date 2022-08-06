928249329ce4c2d24e2b91843e62e27ee49eda3d
Dave Halter <davidhalter88@gmail.com>
2022-08-07 01:01:26 +0200

    Avoid constraint mismatches when they are not

--- zuban_python/src/matching/class_like.rs
+++ zuban_python/src/matching/class_like.rs
@@ -97,7 +97,7 @@ impl<'db, 'a> ClassLike<'db, 'a> {
                 let mut similarity = Match::new_false();
                 match variance {
                     Variance::Covariant => {
-                        for (_, class_like) in c.mro(i_s) {
+                        for (i, (_, class_like)) in c.mro(i_s).enumerate() {
                             let m = self.check_match(
                                 i_s,
                                 matcher.as_deref_mut(),
@@ -107,6 +107,16 @@ impl<'db, 'a> ClassLike<'db, 'a> {
                             if !matches!(m, Match::False(MismatchReason::None)) {
                                 // The other mismatch reasons mean that the class kind of matched,
                                 // but some generics had issues.
+                                if i != 0
+                                    && matches!(
+                                        m,
+                                        Match::False(MismatchReason::ConstraintMismatch { .. })
+                                    )
+                                {
+                                    // It's only a constraint mismatch, if the first did not match
+                                    // the constraints. Otherwise it's just a regular mismatch.
+                                    return Match::new_false();
+                                }
                                 return m;
                             }
                         }
