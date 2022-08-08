7aedb6c9cd51742f1cd139801cbf633c944c919d
Dave Halter <davidhalter88@gmail.com>
2022-08-08 12:04:13 +0200

    Fix an issue with any

--- zuban_python/src/file/utils.rs
+++ zuban_python/src/file/utils.rs
@@ -37,72 +37,68 @@ impl<'db, 'a, 'b> PythonInference<'db, 'a, 'b> {
         result_context
             .with_type_if_exists(self.i_s, |i_s, type_| {
                 let mut found = None;
-                type_
-                    .any(i_s.db, &mut |t| match t {
-                        ClassLike::Class(list_cls)
-                            if list_cls.node_ref == i_s.db.python_state.list() =>
-                        {
-                            let generic_t = list_cls.generics().nth(i_s, 0.into());
-                            let generic_t = Type::from_db_type(i_s.db, &generic_t);
-                            let new_result_context = ResultContext::Known(&generic_t);
+                let maybe = type_.any(i_s.db, &mut |t| match t {
+                    ClassLike::Class(list_cls)
+                        if list_cls.node_ref == i_s.db.python_state.list() =>
+                    {
+                        let generic_t = list_cls.generics().nth(i_s, 0.into());
+                        let generic_t = Type::from_db_type(i_s.db, &generic_t);
+                        let new_result_context = ResultContext::Known(&generic_t);
 
-                            // Since it's a list, now check all the entries if they match the given result
-                            // generic;
-                            if let Some(elements) = list.unpack() {
-                                for (item, element) in elements.enumerate() {
-                                    let check_item = |i_s, inferred, index| {
-                                        generic_t.error_if_not_matches(
-                                            i_s,
-                                            &inferred,
-                                            |i_s, got, expected| {
-                                                NodeRef::new(file, index).add_typing_issue(
-                                                    i_s.db,
-                                                    IssueType::ListItemMismatch {
-                                                        item,
-                                                        got,
-                                                        expected,
-                                                    },
-                                                );
-                                            },
-                                        );
-                                    };
-                                    let mut inference = file.inference(i_s);
-                                    match element {
-                                        StarLikeExpression::NamedExpression(e) => {
-                                            let inferred = inference
-                                                .infer_named_expression_with_context(
-                                                    e,
-                                                    new_result_context,
-                                                );
-                                            check_item(i_s, inferred, e.index())
-                                        }
-                                        StarLikeExpression::Expression(e) => {
-                                            let inferred = inference.infer_expression_with_context(
+                        // Since it's a list, now check all the entries if they match the given result
+                        // generic;
+                        if let Some(elements) = list.unpack() {
+                            for (item, element) in elements.enumerate() {
+                                let check_item = |i_s, inferred, index| {
+                                    generic_t.error_if_not_matches(
+                                        i_s,
+                                        &inferred,
+                                        |i_s, got, expected| {
+                                            NodeRef::new(file, index).add_typing_issue(
+                                                i_s.db,
+                                                IssueType::ListItemMismatch {
+                                                    item,
+                                                    got,
+                                                    expected,
+                                                },
+                                            );
+                                        },
+                                    );
+                                };
+                                let mut inference = file.inference(i_s);
+                                match element {
+                                    StarLikeExpression::NamedExpression(e) => {
+                                        let inferred = inference
+                                            .infer_named_expression_with_context(
                                                 e,
                                                 new_result_context,
                                             );
-                                            check_item(i_s, inferred, e.index())
-                                        }
-                                        StarLikeExpression::StarNamedExpression(e) => {
-                                            todo!()
-                                        }
-                                        StarLikeExpression::StarExpression(e) => {
-                                            todo!()
-                                        }
-                                    };
-                                }
+                                        check_item(i_s, inferred, e.index())
+                                    }
+                                    StarLikeExpression::Expression(e) => {
+                                        let inferred = inference
+                                            .infer_expression_with_context(e, new_result_context);
+                                        check_item(i_s, inferred, e.index())
+                                    }
+                                    StarLikeExpression::StarNamedExpression(e) => {
+                                        todo!()
+                                    }
+                                    StarLikeExpression::StarExpression(e) => {
+                                        todo!()
+                                    }
+                                };
                             }
-
-                            found = Some(t.as_db_type(i_s));
-                            true
                         }
-                        _ => false,
-                    })
-                    .then(|| {
-                        Inferred::new_unsaved_complex(ComplexPoint::TypeInstance(Box::new(
-                            found.unwrap(),
-                        )))
-                    })
+
+                        found = Some(t.as_db_type(i_s));
+                        true
+                    }
+                    _ => false,
+                });
+                // `found` might still be empty, because we matched Any.
+                found.filter(|_| maybe).map(|found| {
+                    Inferred::new_unsaved_complex(ComplexPoint::TypeInstance(Box::new(found)))
+                })
             })
             .flatten()
     }
