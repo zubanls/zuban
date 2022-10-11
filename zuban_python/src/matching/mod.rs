3ef41fb1ccb9814d312a339f6802d105bbb7b5d1
Dave Halter <davidhalter88@gmail.com>
2022-10-11 23:21:21 +0200

    Move the TypeVarMatcher to a different file

--- zuban_python/src/matching/mod.rs
+++ zuban_python/src/matching/mod.rs
@@ -1,18 +1,18 @@
 mod format_data;
 mod generics;
 mod match_;
-mod matcher;
 pub mod params;
 mod result_context;
 mod type_;
+mod type_var_matcher;
 
 pub use format_data::FormatData;
 pub use generics::Generics;
 pub use match_::{ArgumentIndexWithParam, Match, MismatchReason, SignatureMatch};
-pub use matcher::{
-    calculate_callable_type_vars_and_return, calculate_class_init_type_vars_and_return,
-    calculate_function_type_vars_and_return, CalculatedTypeArguments, TypeVarMatcher,
-};
 pub use params::{matches_params, overload_has_overlapping_params};
 pub use result_context::ResultContext;
 pub use type_::Type;
+pub use type_var_matcher::{
+    calculate_callable_type_vars_and_return, calculate_class_init_type_vars_and_return,
+    calculate_function_type_vars_and_return, CalculatedTypeArguments, TypeVarMatcher,
+};
