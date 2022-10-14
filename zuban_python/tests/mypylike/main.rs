ed707444605f065886ab7f8a083fa46df02f2a92
Dave Halter <davidhalter88@gmail.com>
2022-10-14 16:57:13 +0200

    Enable param spec tests, but skipping the individual ones

--- zuban_python/tests/mypylike/main.rs
+++ zuban_python/tests/mypylike/main.rs
@@ -6,7 +6,7 @@ use std::time::Instant;
 
 use regex::{Captures, Regex, Replacer};
 
-const USE_MYPY_TEST_FILES: [&str; 21] = [
+const USE_MYPY_TEST_FILES: [&str; 22] = [
     "check-generics.test",
     "check-generic-alias.test",
     "check-typevar-unbound.test",
@@ -34,7 +34,7 @@ const USE_MYPY_TEST_FILES: [&str; 21] = [
     //"check-union-or-syntax.test",
     //"check-protocols.test",
     //"check-callable.test",
-    //"check-parameter-specification.test",
+    "check-parameter-specification.test",
     //"check-incremental.test",
     //"check-expressions.test",
     //"check-statements.test",
