e11897bd012e47e6aefe3cfe515254d4f927651d
Dave Halter <davidhalter88@gmail.com>
2023-03-01 00:40:20 +0100

    Enable newtype tests and skip many of them

--- zuban_python/tests/mypylike/main.rs
+++ zuban_python/tests/mypylike/main.rs
@@ -10,7 +10,7 @@ use regex::{Captures, Regex, Replacer};
 
 use zuban_python::{DiagnosticConfig, Project, ProjectOptions};
 
-const USE_MYPY_TEST_FILES: [&str; 30] = [
+const USE_MYPY_TEST_FILES: [&str; 31] = [
     "check-generics.test",
     "check-generic-alias.test",
     "check-typevar-unbound.test",
@@ -70,7 +70,7 @@ const USE_MYPY_TEST_FILES: [&str; 30] = [
     //"check-namedtuple.test",
     //"check-class-namedtuple.test",
     //"check-typeddict.test",
-    //"check-newtype.test",
+    "check-newtype.test",
     //"check-unsupported.test",
     //"check-attr.test",
     "check-optional.test",
