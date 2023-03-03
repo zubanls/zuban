d257a7876ea5a4bdb8be6c93ae7a0a43d7a6be31
Dave Halter <davidhalter88@gmail.com>
2023-03-03 12:40:34 +0100

    Simplify maybe_new_type

--- zuban_python/src/value/typing.rs
+++ zuban_python/src/value/typing.rs
@@ -1121,7 +1121,6 @@ fn maybe_new_type<'db>(
     i_s: &mut InferenceState<'db, '_>,
     args: &dyn Arguments<'db>,
 ) -> Option<NewType> {
-    let mut iterator = args.iter_arguments();
     let Some((first, second)) = args.maybe_two_positional_args(i_s.db) else {
         args.as_node_ref().add_typing_issue(
             i_s.db,
@@ -1130,64 +1129,47 @@ fn maybe_new_type<'db>(
         );
         return None
     };
-    if let Some(first_arg) = iterator.next() {
-        let result = if let ArgumentKind::Positional { node_ref, .. } = first_arg.kind {
-            node_ref
-                .as_named_expression()
-                .maybe_single_string_literal()
-                .map(|py_string| (node_ref, py_string))
-        } else {
-            None
-        };
-        let (name_node, py_string) = match result {
-            Some(result) => result,
-            None => {
-                first_arg.as_node_ref().add_typing_issue(
-                    i_s.db,
-                    IssueType::ArgumentIssue(Box::from(
-                        "Argument 1 to NewType(...) must be a string literal",
-                    )),
-                );
-                return None;
-            }
-        };
-        if let Some(name) = py_string.in_simple_assignment() {
-            if name.as_code() != py_string.content() {
-                name_node.add_typing_issue(
-                    i_s.db,
-                    IssueType::TypeVarNameMismatch {
-                        class_name: "NewType",
-                        string_name: Box::from(py_string.content()),
-                        variable_name: Box::from(name.as_code()),
-                    },
-                );
-            }
-        } else {
-            todo!()
+    let result = first
+        .as_named_expression()
+        .maybe_single_string_literal()
+        .map(|py_string| (first, py_string));
+    let (name_node, py_string) = match result {
+        Some(result) => result,
+        None => {
+            first.add_typing_issue(
+                i_s.db,
+                IssueType::ArgumentIssue(Box::from(
+                    "Argument 1 to NewType(...) must be a string literal",
+                )),
+            );
+            return None;
         }
-        let type_node_ref = if let Some(second_arg) = iterator.next() {
-            if let ArgumentKind::Positional { node_ref, .. } = second_arg.kind {
-                NodeRef::new(
-                    node_ref.file,
-                    node_ref.as_named_expression().expression().index(),
-                )
-            } else {
-                return None;
-            }
-        } else {
-            todo!()
-        };
-        Some(NewType::new(
-            PointLink {
-                file: name_node.file_index(),
-                node_index: py_string.index(),
-            },
-            type_node_ref.as_link(),
-        ))
+    };
+    if let Some(name) = py_string.in_simple_assignment() {
+        if name.as_code() != py_string.content() {
+            name_node.add_typing_issue(
+                i_s.db,
+                IssueType::TypeVarNameMismatch {
+                    class_name: "NewType",
+                    string_name: Box::from(py_string.content()),
+                    variable_name: Box::from(name.as_code()),
+                },
+            );
+        }
     } else {
-        todo!();
-        //None
+        todo!()
     }
+    let type_node_ref = NodeRef::new(
+        second.file,
+        second.as_named_expression().expression().index(),
+    );
+    Some(NewType::new(
+        PointLink {
+            file: name_node.file_index(),
+            node_index: py_string.index(),
+        },
+        type_node_ref.as_link(),
+    ))
 }
 
 #[derive(Debug)]
