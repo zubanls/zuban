56bd0c29146a620f02bf04e62d46f46682150583
Dave Halter <davidhalter88@gmail.com>
2023-04-24 00:25:32 +0200

    Enable semanal-literal.test

--- zuban_python/tests/mypylike/main.rs
+++ zuban_python/tests/mypylike/main.rs
@@ -10,7 +10,7 @@ use regex::{Captures, Regex, Replacer};
 
 use zuban_python::{DiagnosticConfig, Project, ProjectOptions};
 
-const USE_MYPY_TEST_FILES: [&str; 37] = [
+const USE_MYPY_TEST_FILES: [&str; 38] = [
     // Semanal tests
     //"semanal-abstractclasses.test",
     "semanal-basic.test",
@@ -20,7 +20,7 @@ const USE_MYPY_TEST_FILES: [&str; 37] = [
     "semanal-errors.test",
     //"semanal-expressions.test",
     //"semanal-lambda.test",
-    //"semanal-literal.test",
+    "semanal-literal.test",
     //"semanal-modules.test",
     "semanal-namedtuple.test",
     //"semanal-python310.test",
