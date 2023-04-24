82c79889193432c9c2441c0591c714af771a3ad6
Dave Halter <davidhalter88@gmail.com>
2023-04-24 18:22:10 +0200

    Add a few more semanal tests for match that are currently failing

--- zuban_python/tests/mypylike/main.rs
+++ zuban_python/tests/mypylike/main.rs
@@ -10,13 +10,13 @@ use regex::{Captures, Regex, Replacer};
 
 use zuban_python::{DiagnosticConfig, Project, ProjectOptions};
 
-const USE_MYPY_TEST_FILES: [&str; 45] = [
+const USE_MYPY_TEST_FILES: [&str; 46] = [
     // Semanal tests
     //"semanal-abstractclasses.test",
     "semanal-basic.test",
     "semanal-classes.test",
     //"semanal-classvar.test",
-    //"semanal-errors-python310.test",
+    "semanal-errors-python310.test",
     "semanal-errors.test",
     //"semanal-expressions.test",
     "semanal-lambda.test",
