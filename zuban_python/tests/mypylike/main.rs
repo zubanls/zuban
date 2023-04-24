ea0de14945603f045226f8dd4b64f8c9ef720eae
Dave Halter <davidhalter88@gmail.com>
2023-04-24 18:19:34 +0200

    Add match statement tests and skip them

--- zuban_python/tests/mypylike/main.rs
+++ zuban_python/tests/mypylike/main.rs
@@ -10,7 +10,7 @@ use regex::{Captures, Regex, Replacer};
 
 use zuban_python::{DiagnosticConfig, Project, ProjectOptions};
 
-const USE_MYPY_TEST_FILES: [&str; 44] = [
+const USE_MYPY_TEST_FILES: [&str; 45] = [
     // Semanal tests
     //"semanal-abstractclasses.test",
     "semanal-basic.test",
@@ -23,7 +23,7 @@ const USE_MYPY_TEST_FILES: [&str; 44] = [
     "semanal-literal.test",
     //"semanal-modules.test",
     "semanal-namedtuple.test",
-    //"semanal-python310.test",
+    "semanal-python310.test",
     "semanal-statements.test",
     "semanal-typealiases.test",
     //"semanal-typeddict.test",
