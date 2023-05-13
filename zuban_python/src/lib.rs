8c56cd6ef9a455887f9578c94f07d5d571c909f3
Dave Halter <davidhalter88@gmail.com>
2023-05-13 13:54:00 +0200

    Move ValueKind

--- zuban_python/src/lib.rs
+++ zuban_python/src/lib.rs
@@ -26,7 +26,6 @@ use inference_state::InferenceState;
 use inferred::Inferred;
 use name::Names;
 use parsa_python_ast::CodeIndex;
-pub use value::ValueKind;
 
 pub struct Project {
     db: Database,
@@ -268,6 +267,38 @@ impl<'a> Drop for Script<'a> {
     fn drop(&mut self) {
         self.project.db.release()
     }
+}
+
+#[derive(Debug, Eq, PartialEq)]
+pub enum ValueKind {
+    Unknown = 0,
+    // Taken from LSP, unused kinds are commented
+    //File = 1,
+    Module = 2,
+    Namespace = 3,
+    //Package = 4,
+    Class = 5,
+    Method = 6,
+    Property = 7,
+    Field = 8,
+    //Constructor = 9,
+    //Enum = 10,
+    //Interface = 11,
+    Function = 12,
+    //Variable = 13,
+    Constant = 14,
+    String = 15,
+    Number = 16,
+    Bool = 17,
+    Array = 18,
+    Object = 19, // From JavaScript objects -> Basically an instance
+    //Key = 20,
+    Null = 21,
+    //EnumMember = 22,
+    //Struct = 23,
+    //Event = 24,
+    //Operator = 25,
+    TypeParameter = 26,
 }
 
 #[cfg(test)]
