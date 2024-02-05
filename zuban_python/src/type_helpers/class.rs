bd73411f0688fe00c02948c9600bf5cbf9386484
Dave Halter <davidhalter88@gmail.com>
2024-02-05 15:35:37 +0100

    Implement __init__ type vars + class type vars as callable

--- zuban_python/src/type_helpers/class.rs
+++ zuban_python/src/type_helpers/class.rs
@@ -2609,9 +2609,6 @@ impl NewOrInitConstructor<'_> {
             let to_callable = |c: &CallableContent| {
                 // Since __init__ does not have a return, We need to check the params
                 // of the __init__ functions and the class as a return type separately.
-                if !c.type_vars.is_empty() {
-                    todo!()
-                }
                 c.remove_first_param().map(|mut c| {
                     let self_ = cls.as_type(i_s.db);
                     let needs_type_var_remap = self.init_class.is_some_and(|c| {
@@ -2641,25 +2638,37 @@ impl NewOrInitConstructor<'_> {
                     c.return_type = self_;
 
                     // Now we have two sets of generics, the generics for the class and the
-                    // generics for the __init__ function.
-                    // TODO merge those
-                    let type_vars = cls.type_vars(i_s);
-                    if !type_vars.is_empty() {
+                    // generics for the __init__ function. We merge those and then set the proper
+                    // ids for the type vars.
+                    let class_type_vars = cls.type_vars(i_s);
+                    if class_type_vars.is_empty() {
+                        c.type_vars = class_type_vars.clone();
+                    } else {
+                        let mut tv_vec = class_type_vars.as_vec();
+                        tv_vec.extend(c.type_vars.iter().cloned());
+                        c.type_vars = TypeVarLikes::from_vec(tv_vec);
+                    }
+                    if !c.type_vars.is_empty() {
                         c = c.replace_type_var_likes_and_self(
                             i_s.db,
                             &mut |mut usage| {
-                                if cls.node_ref.as_link() == usage.in_definition() {
+                                let in_def = usage.in_definition();
+                                if cls.node_ref.as_link() == in_def {
                                     usage.update_in_definition_and_index(
                                         c.defined_at,
                                         usage.index(),
                                     );
+                                } else if c.defined_at == in_def {
+                                    usage.update_in_definition_and_index(
+                                        c.defined_at,
+                                        (class_type_vars.len() + usage.index().as_usize()).into(),
+                                    )
                                 }
                                 usage.into_generic_item()
                             },
                             &|| unreachable!("was already replaced above"),
                         );
                     }
-                    c.type_vars = type_vars.clone();
                     c
                 })
             };
