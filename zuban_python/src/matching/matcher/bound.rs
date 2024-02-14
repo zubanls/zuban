9ed4d1d215282c7623965209e5464ebfde2b819f
Dave Halter <davidhalter88@gmail.com>
2024-02-14 02:22:24 +0100

    Start working on Bound2

--- zuban_python/src/matching/matcher/bound.rs
+++ zuban_python/src/matching/matcher/bound.rs
@@ -279,16 +279,311 @@ impl Bound {
     }
 }
 
-/*
 #[derive(Debug, Clone, PartialEq)]
-enum BoundKind {
-    TypeVar(Type),
-    TypeVarTuple(TupleArgs),
-    ParamSpec(CallableParams),
+enum Bound2 {
+    Uncalculated { fallback: Option<Type> },
 
     Invariant(BoundKind),
     Upper(BoundKind),
     UpperAndLower(BoundKind, BoundKind),
     Lower(BoundKind),
 }
-*/
+
+#[derive(Debug, Clone, PartialEq)]
+enum BoundKind {
+    TypeVar(Type),
+    TypeVarTuple(TupleArgs),
+    ParamSpec(CallableParams),
+}
+
+impl Bound2 {
+    pub fn merge(&mut self, db: &Database, other: Self) -> Match {
+        if self == &other {
+            return Match::new_true();
+        }
+        let i_s = InferenceState::new(db);
+        let mut m = Match::new_true();
+        if let Self::Uncalculated { fallback } = self {
+            if !matches!(&other, Self::Uncalculated { .. }) || fallback.is_none() {
+                *self = other;
+            }
+            return Match::new_true();
+        }
+        let (t, variance) = match other {
+            Self::Upper(t) => (t, Variance::Contravariant),
+            Self::Lower(t) => (t, Variance::Covariant),
+            Self::Invariant(t) => (t, Variance::Invariant),
+            Self::UpperAndLower(upper, t) => {
+                m = self.merge_or_mismatch(&i_s, &upper, Variance::Contravariant);
+                (t, Variance::Covariant)
+            }
+            Self::Uncalculated { .. } => return Match::new_true(),
+        };
+        m & self.merge_or_mismatch(&i_s, &t, variance)
+    }
+
+    fn merge_or_mismatch2(
+        &mut self,
+        i_s: &InferenceState,
+        other: &BoundKind,
+        variance: Variance,
+    ) -> Match {
+        /*
+        match (self, other) {
+            (Self::TypeVar(bound1), Self::TypeVar(bound2)) => {
+            }
+            (Self::TypeVarTuple(tup1), Self::TypeVarTuple(tup2)) => {
+                dbg!(tup1, tup2);
+                //match_tuple_type_arguments(&i_s, &mut Matcher::default(), tup1, &tup2, Variance::Invariant);
+                todo!()
+            }
+            (Self::ParamSpec(p1), Self::ParamSpec(p2)) => {
+                //matches_params(&i_s, &mut Matcher::default(), p1, &p2, Variance::Invariant, false)
+                dbg!(p1, p2);
+                todo!()
+            }
+        }
+        */
+        todo!()
+    }
+    pub fn merge_or_mismatch(
+        &mut self,
+        i_s: &InferenceState,
+        other: &BoundKind,
+        variance: Variance,
+    ) -> Match {
+        // First check if the value is between the bounds.
+        let matches = match self {
+            Self::Invariant(t) => {
+                let m = t.is_simple_same_type(i_s, other);
+                if m.bool() {
+                    return m; // In the false case we still have to check for the variance cases.
+                }
+                m
+            }
+            Self::Upper(lower) => lower.is_simple_super_type_of(i_s, other),
+            Self::Lower(upper) => upper.is_simple_sub_type_of(i_s, other),
+            Self::UpperAndLower(lower, upper) => {
+                lower.is_simple_super_type_of(i_s, other) & upper.is_simple_sub_type_of(i_s, other)
+            }
+            Self::Uncalculated { .. } => unreachable!(),
+        };
+        if matches.bool() {
+            // If we are between the bounds we might need to update lower/upper bounds
+            match variance {
+                Variance::Invariant => *self = Self::Invariant(other.clone()),
+                Variance::Covariant => self.update_lower_bound(i_s, other),
+                Variance::Contravariant => self.update_upper_bound(i_s, other),
+            }
+        } else {
+            // If we are not between the lower and upper bound, but the value is co or
+            // contravariant, it can still be valid.
+            match variance {
+                Variance::Invariant => (),
+                Variance::Covariant => match self {
+                    Self::Lower(t) => {
+                        // TODO shouldn't this also do a limited common base type search in the
+                        // case of LowerAndUpper?
+                        let m = t.is_simple_super_type_of(i_s, other);
+                        *t = t.common_base_type(i_s, other);
+                        if !m.bool() {
+                            return Match::new_true();
+                        }
+                        return m;
+                    }
+                    Self::Invariant(t) | Self::UpperAndLower(_, t) => {
+                        return t.is_simple_super_type_of(i_s, other)
+                    }
+                    Self::Upper(t) => {}
+                    Self::Uncalculated { .. } => unreachable!(),
+                },
+                Variance::Contravariant => match self {
+                    Self::Upper(t) => {
+                        // TODO shouldn't we also check LowerAndUpper like this?
+                        let m = t.is_simple_sub_type_of(i_s, other);
+                        if !m.bool() {
+                            *t = t.common_sub_type(i_s, other);
+                            return Match::new_true();
+                        }
+                        return m;
+                    }
+                    Self::Invariant(t) | Self::UpperAndLower(t, _) => {
+                        return t.is_simple_sub_type_of(i_s, other);
+                    }
+                    Self::Lower(_) => {}
+                    Self::Uncalculated { .. } => unreachable!(),
+                },
+            };
+        }
+        matches
+    }
+
+    fn update_upper_bound(&mut self, i_s: &InferenceState, upper: &BoundKind) {
+        match self {
+            Self::Upper(old) => *self = Self::Upper(upper.clone()),
+            Self::Lower(lower) | Self::UpperAndLower(_, lower) => {
+                *self = Self::UpperAndLower(upper.clone(), lower.clone())
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    fn update_lower_bound(&mut self, i_s: &InferenceState, lower: &BoundKind) {
+        match self {
+            Self::Lower(old) => *self = Self::Lower(old.common_base_type(i_s, lower)),
+            Self::Upper(upper) => *self = Self::UpperAndLower(upper.clone(), lower.clone()),
+            Self::UpperAndLower(upper, old) => {
+                *self = Self::UpperAndLower(upper.clone(), old.common_base_type(i_s, lower))
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn search_type_vars<C: FnMut(TypeVarLikeUsage) + ?Sized>(&self, found_type_var: &mut C) {
+        let t = match self {
+            Self::Invariant(t) | Self::Lower(t) | Self::Upper(t) => t,
+            Self::UpperAndLower(upper, lower) => {
+                upper.search_type_vars(found_type_var);
+                lower
+            }
+            Self::Uncalculated { fallback: Some(t) } => {
+                t.search_type_vars(found_type_var);
+                return;
+            }
+            Self::Uncalculated { fallback: None } => return,
+        };
+        t.search_type_vars(found_type_var)
+    }
+
+    pub fn replace_type_var_likes(
+        &self,
+        db: &Database,
+        on_type_var_like: &mut impl FnMut(TypeVarLikeUsage) -> GenericItem,
+    ) -> Self {
+        match self {
+            Self::Invariant(t) => Self::Invariant(t.replace_type_var_likes(db, on_type_var_like)),
+            Self::Upper(t) => Self::Upper(t.replace_type_var_likes(db, on_type_var_like)),
+            Self::Lower(t) => Self::Upper(t.replace_type_var_likes(db, on_type_var_like)),
+            Self::UpperAndLower(upper, lower) => Self::UpperAndLower(
+                upper.replace_type_var_likes(db, on_type_var_like),
+                lower.replace_type_var_likes(db, on_type_var_like),
+            ),
+            Self::Uncalculated { fallback: Some(t) } => Self::Uncalculated {
+                fallback: Some(t.replace_type_var_likes(db, on_type_var_like)),
+            },
+            Self::Uncalculated { fallback: None } => Self::Uncalculated { fallback: None },
+        }
+    }
+
+    pub fn as_generic_item(
+        &self,
+        db: &Database,
+        on_uncalculated: impl FnOnce() -> GenericItem,
+    ) -> GenericItem {
+        match self {
+            // If the upper bound is a literal, we do not want to use the lower bound.
+            Self::UpperAndLower(t @ BoundKind::TypeVar(Type::Literal(_)), _) => t.as_generic_item(),
+            Self::Lower(BoundKind::TypeVar(Type::Literal(l)))
+            | Self::UpperAndLower(_, BoundKind::TypeVar(Type::Literal(l)))
+                if l.implicit =>
+            {
+                GenericItem::TypeArg(db.python_state.literal_type(&l.kind))
+            }
+            Self::Invariant(k) | Self::Upper(k) | Self::Lower(k) | Self::UpperAndLower(_, k) => {
+                k.as_generic_item()
+            }
+            // Any is just ignored by the context later.
+            Self::Uncalculated { .. } => on_uncalculated(),
+        }
+    }
+}
+
+impl BoundKind {
+    fn is_simple_same_type(&self, i_s: &InferenceState, other: &Self) -> Match {
+        match (self, other) {
+            (Self::TypeVar(t1), Self::TypeVar(t2)) => t1.is_simple_same_type(i_s, t2),
+            (Self::TypeVarTuple(tup1), Self::TypeVarTuple(tup2)) => todo!(),
+            (Self::ParamSpec(params1), Self::ParamSpec(params2)) => todo!(),
+            _ => unreachable!(),
+        }
+    }
+
+    fn is_simple_super_type_of(&self, i_s: &InferenceState, other: &Self) -> Match {
+        match (self, other) {
+            (Self::TypeVar(t1), Self::TypeVar(t2)) => t1.is_simple_super_type_of(i_s, t2),
+            (Self::TypeVarTuple(tup1), Self::TypeVarTuple(tup2)) => todo!(),
+            (Self::ParamSpec(params1), Self::ParamSpec(params2)) => todo!(),
+            _ => unreachable!(),
+        }
+    }
+
+    fn is_simple_sub_type_of(&self, i_s: &InferenceState, other: &Self) -> Match {
+        match (self, other) {
+            (Self::TypeVar(t1), Self::TypeVar(t2)) => t1.is_simple_sub_type_of(i_s, t2),
+            (Self::TypeVarTuple(tup1), Self::TypeVarTuple(tup2)) => todo!(),
+            (Self::ParamSpec(params1), Self::ParamSpec(params2)) => todo!(),
+            _ => unreachable!(),
+        }
+    }
+
+    fn common_base_type(&self, i_s: &InferenceState, other: &Self) -> Self {
+        match (self, other) {
+            (Self::TypeVar(t1), Self::TypeVar(t2)) => Self::TypeVar(t1.common_base_type(i_s, t2)),
+            (Self::TypeVarTuple(tup1), Self::TypeVarTuple(tup2)) => todo!(),
+            (Self::ParamSpec(params1), Self::ParamSpec(params2)) => todo!(),
+            _ => unreachable!(),
+        }
+    }
+
+    fn common_sub_type(&self, i_s: &InferenceState, other: &Self) -> Self {
+        match (self, other) {
+            (Self::TypeVar(t1), Self::TypeVar(t2)) => {
+                Self::TypeVar(t1.common_sub_type(i_s, t2).unwrap_or(Type::Never))
+            }
+            (Self::TypeVarTuple(tup1), Self::TypeVarTuple(tup2)) => todo!(),
+            (Self::ParamSpec(params1), Self::ParamSpec(params2)) => todo!(),
+            _ => unreachable!(),
+        }
+    }
+
+    fn search_type_vars<C: FnMut(TypeVarLikeUsage) + ?Sized>(&self, found_type_var: &mut C) {
+        match self {
+            BoundKind::TypeVar(t) => t.search_type_vars(found_type_var),
+            BoundKind::TypeVarTuple(tup) => tup.search_type_vars(found_type_var),
+            BoundKind::ParamSpec(params) => params.search_type_vars(found_type_var),
+        }
+    }
+
+    fn as_generic_item(&self) -> GenericItem {
+        match self {
+            Self::TypeVar(t) => GenericItem::TypeArg(t.clone()),
+            Self::TypeVarTuple(ts) => GenericItem::TypeArgs(TypeArgs::new(ts.clone())),
+            Self::ParamSpec(param_spec) => GenericItem::ParamSpecArg(ParamSpecArg {
+                params: param_spec.clone(),
+                type_vars: None,
+            }),
+        }
+    }
+
+    pub fn replace_type_var_likes(
+        &self,
+        db: &Database,
+        on_type_var_like: &mut impl FnMut(TypeVarLikeUsage) -> GenericItem,
+    ) -> Self {
+        match self {
+            Self::TypeVar(t) => Self::TypeVar(t.replace_type_var_likes(db, on_type_var_like)),
+            Self::TypeVarTuple(tup) => Self::TypeVarTuple(tup.replace_type_var_likes_and_self(
+                db,
+                on_type_var_like,
+                &|| Type::Self_,
+            )),
+            Self::ParamSpec(params) => Self::ParamSpec(
+                params
+                    .replace_type_var_likes_and_self(db, &mut None, None, on_type_var_like, &|| {
+                        Type::Self_
+                    })
+                    .0,
+            ),
+        }
+    }
+}
