6b9d55b6d4054bbc0c1a7bb36f3401ae03279bd1
Dave Halter <davidhalter88@gmail.com>
2024-07-23 10:20:28 +0200

    Implement reverse intersections when they are necessary

--- zuban_python/src/type_/intersection.rs
+++ zuban_python/src/type_/intersection.rs
@@ -94,10 +94,10 @@ impl Intersection {
         }
 
         //Subclass of "C", "B", and "A" cannot exist: would have incompatible method signatures
-        let intersection = Self::from_types(t1.clone(), t2.clone());
+        let mut intersection = Self::from_types(t1.clone(), t2.clone());
 
         let mut had_issue = false;
-        let fmt_intersection = || {
+        let fmt_intersection = |intersection: &Self| {
             intersection
                 .format_names(&FormatData::new_short(i_s.db), true)
                 .into()
@@ -106,7 +106,7 @@ impl Intersection {
             if let Some(cls) = t.maybe_class(i_s.db) {
                 if cls.use_cached_class_infos(i_s.db).is_final {
                     add_issue(IssueKind::IntersectionCannotExistDueToFinalClass {
-                        intersection: fmt_intersection(),
+                        intersection: fmt_intersection(&intersection),
                         final_class: cls.name().into(),
                     });
                     had_issue = true;
@@ -120,27 +120,45 @@ impl Intersection {
         let linearizable = linearize_mro_and_return_linearizable(i_s, &intersection.entries).1;
         if !linearizable {
             add_issue(IssueKind::IntersectionCannotExistDueToInconsistentMro {
-                intersection: fmt_intersection(),
+                intersection: fmt_intersection(&intersection),
             });
             return Err(());
         }
 
-        check_multiple_inheritance(
-            i_s,
-            || {
-                intersection.iter().map(|t| match t.maybe_class(i_s.db) {
-                    Some(c) => TypeOrClass::Class(c),
-                    None => TypeOrClass::Type(Cow::Borrowed(t)),
-                })
-            },
-            |_| true,
-            |_| had_issue = true,
-        );
+        let check_multi_inheritance = |intersection: &Self, had_issue: &mut _| {
+            check_multiple_inheritance(
+                i_s,
+                || {
+                    intersection.iter().map(|t| match t.maybe_class(i_s.db) {
+                        Some(c) => TypeOrClass::Class(c),
+                        None => TypeOrClass::Type(Cow::Borrowed(t)),
+                    })
+                },
+                |_| true,
+                |_| *had_issue = true,
+            );
+        };
+
+        check_multi_inheritance(&intersection, &mut had_issue);
+        if had_issue {
+            // Try the other direction, because that might be valid.
+            // Other checks are not important anymore, because they don't depend on direction.
+            // (The linearization check of course does, but only if a class is a subtype of another
+            // one, which shouldn't be the case here anymore).
+            had_issue = false;
+            let old_intersection = intersection;
+            intersection = Self::from_types(t2.clone(), t1.clone());
+            check_multi_inheritance(&intersection, &mut had_issue);
+            if had_issue {
+                // Reset to the original intersection to always have the same order of error messages
+                intersection = old_intersection;
+            }
+        }
 
         if had_issue {
             add_issue(
                 IssueKind::IntersectionCannotExistDueToIncompatibleMethodSignatures {
-                    intersection: fmt_intersection(),
+                    intersection: fmt_intersection(&intersection),
                 },
             );
             return Err(());
