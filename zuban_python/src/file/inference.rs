55c5b2fd02c280f30006a9466dfbab4471d0ee69
Dave Halter <davidhalter88@gmail.com>
2024-08-31 19:26:37 +0200

    Avoid inferring a target if it is already calculating

--- zuban_python/src/file/inference.rs
+++ zuban_python/src/file/inference.rs
@@ -942,7 +942,9 @@ impl<'db, 'file, 'i_s> Inference<'db, 'file, 'i_s> {
                 let name_def_ref =
                     NodeRef::new(self.file, first_index - NAME_DEF_TO_NAME_DIFFERENCE);
                 let point = name_def_ref.point();
-                if point.calculated() && point.maybe_specific().is_some_and(|s| s.is_partial()) {
+                if point.calculated() && point.maybe_specific().is_some_and(|s| s.is_partial())
+                    || point.calculating()
+                {
                     return None;
                 }
                 if narrow {
