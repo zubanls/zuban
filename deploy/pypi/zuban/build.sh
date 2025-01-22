c02c26608650e843a2a495a718dad2e3b8f5b6e7
Dave Halter <davidhalter88@gmail.com>
2025-01-22 11:35:07 +0100

    Implement a build process where version numbers can be provided

--- deploy/pypi/zuban/build.sh
+++ deploy/pypi/zuban/build.sh
@@ -3,7 +3,24 @@ set -eu -o pipefail
 
 cd "$(dirname "$0")"
 
+if [[ -z "${2:-}" ]]; then
+  echo "Error: Needs at least two arguments, like 'build.sh 0.0.1 develop'!"
+  exit 1
+fi
+
+version="$1"
+maturin_command="$2"
+
+# Only check if something looks like a version number. This is just an
+# assertion, correct version checking should be done somewhere else.
+if [[ ! "$version" =~ ^[0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  echo "Error: '$version' does not seem to be a valid version!"
+  exit 1
+fi
+
 cargo install cargo-about || true
 cargo about generate -o licenses.html about.hbs --fail --manifest-path ../../../Cargo.toml
 
-maturin "$1" --ignore-rust-version
+sed "s/<ZubanVersionTag>/$version/" < pyproject.toml.template > pyproject.toml
+
+maturin "$maturin_command" --ignore-rust-version
