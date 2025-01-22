e5541afc120814c7e84841a220b8d9a88d288d67
Dave Halter <davidhalter88@gmail.com>
2025-01-22 21:34:55 +0100

    Fix building with maturin

--- deploy/pypi/zuban/build.sh
+++ deploy/pypi/zuban/build.sh
@@ -5,15 +5,8 @@ cd "$(dirname "$0")"
 
 BASE_DIR="../../.."
 WORKSPACE_TOML="$BASE_DIR/Cargo.toml"
-ZUBAN_TOML="$BASE_DIR/crates/zuban/Cargo.toml"
 
-if [[ -z "${1:-}" ]]; then
-  echo "Error: Needs at least one argument, like 'build.sh develop'!"
-  exit 1
-fi
-
-cargo install cargo-about || true
+cargo install cargo-about
 cargo about generate -o licenses.html about.hbs --fail --manifest-path "$WORKSPACE_TOML" --offline
-
 
 maturin publish --ignore-rust-version --no-sdist
