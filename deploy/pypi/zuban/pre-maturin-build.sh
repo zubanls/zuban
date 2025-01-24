3d3a4264e63ac8603f6ef049e6fc0248b65eaa2d
Dave Halter <davidhalter88@gmail.com>
2025-01-24 19:47:00 +0100

    Try to fix publishing to PyPI

--- deploy/pypi/zuban/pre-maturin-build.sh
+++ deploy/pypi/zuban/pre-maturin-build.sh
@@ -6,5 +6,6 @@ cd "$(dirname "$0")"
 BASE_DIR="../../.."
 WORKSPACE_TOML="$BASE_DIR/Cargo.toml"
 
-cargo install cargo-about
+# Use --debug to speed up compilation
+cargo install --locked --debug cargo-about
 cargo about generate -o licenses.html about.hbs --fail --manifest-path "$WORKSPACE_TOML"
