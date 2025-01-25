afeb143df0bdaeb17acac73e244a81e1b63a201e
Dave Halter <davidhalter88@gmail.com>
2025-01-25 22:17:52 +0100

    Make the panic hook a cell instead of a RefCell, it feels better this way and can never panic

--- crates/zubanls/src/panic_hooks.rs
+++ crates/zubanls/src/panic_hooks.rs
@@ -1,12 +1,16 @@
-use std::{cell::RefCell, panic::PanicHookInfo};
+use std::{cell::Cell, panic::PanicHookInfo};
 
 type PanicHook = Box<dyn Fn(&PanicHookInfo<'_>) + 'static>;
+
+thread_local! {
+    static HOOK: Cell<Option<PanicHook>> = const { Cell::new(None) };
+}
 
 pub struct RestorePanicHook();
 
 impl Drop for RestorePanicHook {
     fn drop(&mut self) {
-        with_hook(|x| *x = None)
+        HOOK.with(|cell| cell.take());
     }
 }
 
@@ -14,20 +18,13 @@ pub fn enter(new_hook: PanicHook) -> RestorePanicHook {
     // Remove the previous hook and set a new one for now
     let default_hook = std::panic::take_hook();
     std::panic::set_hook(Box::new(move |panic_info| {
-        with_hook(|maybe_custom_hook| match maybe_custom_hook {
+        HOOK.with(|cell| match cell.take() {
             Some(new_hook) => new_hook(panic_info),
             None => default_hook(panic_info),
         })
     }));
-    with_hook(move |maybe_custom_hook| {
-        *maybe_custom_hook = Some(new_hook);
+    HOOK.with(move |cell| {
+        cell.set(Some(new_hook));
     });
     RestorePanicHook()
-}
-
-fn with_hook(f: impl FnOnce(&mut Option<PanicHook>)) {
-    thread_local! {
-        static CTX: RefCell<Option<PanicHook>> = const { RefCell::new(None) };
-    }
-    CTX.with(|ctx| f(&mut ctx.borrow_mut()));
 }
