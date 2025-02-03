ba9998e5325c4eeb1f9adca9fc615e031ea27c8d
Dave Halter <davidhalter88@gmail.com>
2025-02-03 12:09:11 +0100

    Try to fix windows

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -6,5 +6,8 @@ rustup show
 
 RUST_BACKTRACE=1 cargo test --locked
 
+# On Windows pipefail causes problems with the command below, so deactivate it temporarily
+set +o pipefail
 # Check that the server ends, because no license was found
 (cargo run --bin zubanls 2>&1 || true) | tee >(cat >&2) | grep -Eq 'license.json": (No such file or directory|The system cannot find the path)'
+set -o pipefail
