db312640f8d796f3ec2e7ff45dc74c415db45066
Dave Halter <davidhalter88@gmail.com>
2025-02-12 23:37:39 +0100

    Simply ignore on unrecognized Mypy options instead of panicing

--- crates/config/src/lib.rs
+++ crates/config/src/lib.rs
@@ -591,7 +591,12 @@ fn set_bool_init_flags(
         "cache_fine_grained" => (),
         "ignore_errors" => return value.as_bool(invert),
         "python_version" => bail!("python_version not supported in inline configuration"),
-        _ => bail!("Unrecognized option: {original_name} = {}", value.as_repr()),
+        _ => {
+            tracing::error!(
+                "Unrecognized option {original_name} = {} in Mypy config",
+                value.as_repr()
+            );
+        }
     }
     Ok(false)
 }
