78ab4346d957164a6e9d3e0fd571729578c38157
Dave Halter <davidhalter88@gmail.com>
2025-03-24 11:17:44 +0100

    Move type application code

---
+++ crates/zuban_python/src/file/type_computation/application.rs
@@ -0,0 +1,245 @@
+// For Type Application, e.g. x = list[int]()
+use std::rc::Rc;
+
+use super::super::name_resolution::NameResolution;
+use super::{TypeComputation, TypeComputationOrigin, TypeContent, TypeVarCallbackReturn};
+use crate::{
+    database::{ComplexPoint, Specific, TypeAlias},
+    diagnostics::IssueKind,
+    getitem::SliceType,
+    inference_state::InferenceState,
+    inferred::Inferred,
+    type_::{Dataclass, NamedTuple, Type, TypeVarLike, TypedDict},
+    type_helpers::Class,
+};
+
+macro_rules! compute_type_application {
+    ($self:ident, $slice_type:expr, $from_alias_definition:expr, $method:ident $args:tt) => {{
+        let mut on_type_var = |i_s: &InferenceState, _: &_, type_var_like: TypeVarLike, current_callable: Option<_>| {
+            if let Some(result) = i_s.find_parent_type_var(&type_var_like) {
+                if $from_alias_definition {
+                    $slice_type.as_node_ref().add_issue(
+                        i_s,
+                        IssueKind::BoundTypeVarInAlias{
+                            name: Box::from(type_var_like.name(i_s.db))
+                        },
+                    );
+                } else {
+                    return result
+                }
+            }
+            if $from_alias_definition || current_callable.is_some(){
+                TypeVarCallbackReturn::NotFound { allow_late_bound_callables: !$from_alias_definition }
+            } else {
+                TypeVarCallbackReturn::UnboundTypeVar
+            }
+        };
+        let mut tcomp = TypeComputation::new(
+            $self.i_s,
+            $self.file,
+            $slice_type.as_node_ref().as_link(),
+            &mut on_type_var,
+            match $from_alias_definition {
+                false => TypeComputationOrigin::TypeApplication,
+                true => TypeComputationOrigin::TypeAlias,
+            }
+        );
+        let t = tcomp.$method $args;
+        match t {
+            TypeContent::SimpleGeneric{class_link, generics, ..} => {
+                Inferred::from_type(Type::Type(Rc::new(Type::new_class(class_link, generics))))
+            }
+            TypeContent::Type(mut type_) => {
+                let type_var_likes = tcomp.into_type_vars(|_, recalculate_type_vars| {
+                    type_ = recalculate_type_vars(&type_);
+                });
+                if type_var_likes.len() > 0 && $from_alias_definition  {
+                    debug_assert!($from_alias_definition);
+                    Inferred::new_unsaved_complex(ComplexPoint::TypeAlias(Box::new(TypeAlias::new_valid(
+                        type_var_likes,
+                        $slice_type.as_node_ref().as_link(),
+                        None,
+                        Rc::new(type_),
+                        false,
+                    ))))
+                } else {
+                    Inferred::from_type(Type::Type(Rc::new(type_)))
+                }
+            },
+            TypeContent::Unknown(cause) => Inferred::new_any(cause.into()),
+            TypeContent::InvalidVariable(var) => {
+                var.add_issue(
+                    $self.i_s.db,
+                    |issue| $slice_type.as_node_ref().add_issue($self.i_s, issue),
+                    tcomp.origin
+                );
+                Inferred::new_any_from_error()
+            }
+            _ => {
+                // Currently this is only the case with Annotated. Not sure if this is correct in
+                // the future, but for now returning typing._SpecialForm seems fine.
+                Inferred::from_type($self.i_s.db.python_state.typing_special_form_type())
+            }
+        }
+    }}
+}
+
+impl<'db, 'file> NameResolution<'db, 'file, '_> {
+    pub(crate) fn compute_type_application_on_class(
+        &self,
+        class: Class,
+        slice_type: SliceType,
+        from_alias_definition: bool,
+    ) -> Inferred {
+        compute_type_application!(
+            self,
+            slice_type,
+            from_alias_definition,
+            compute_type_get_item_on_class(class, slice_type, None)
+        )
+    }
+
+    pub(crate) fn compute_type_application_on_dataclass(
+        &self,
+        dataclass: &Dataclass,
+        slice_type: SliceType,
+        from_alias_definition: bool,
+    ) -> Inferred {
+        compute_type_application!(
+            self,
+            slice_type,
+            from_alias_definition,
+            compute_type_get_item_on_dataclass(dataclass, slice_type, None)
+        )
+    }
+
+    pub(crate) fn compute_type_application_on_named_tuple(
+        &self,
+        named_tuple: Rc<NamedTuple>,
+        slice_type: SliceType,
+        from_alias_definition: bool,
+    ) -> Inferred {
+        compute_type_application!(
+            self,
+            slice_type,
+            from_alias_definition,
+            compute_type_get_item_on_named_tuple(named_tuple, slice_type)
+        )
+    }
+
+    pub(crate) fn compute_type_application_on_typed_dict(
+        &self,
+        typed_dict: &TypedDict,
+        slice_type: SliceType,
+        from_alias_definition: bool,
+    ) -> Inferred {
+        compute_type_application!(
+            self,
+            slice_type,
+            from_alias_definition,
+            compute_type_get_item_on_typed_dict(typed_dict, slice_type)
+        )
+    }
+
+    pub(crate) fn compute_type_application_on_alias(
+        &self,
+        alias: &TypeAlias,
+        slice_type: SliceType,
+        from_alias_definition: bool,
+    ) -> Inferred {
+        if !from_alias_definition && !alias.application_allowed() {
+            slice_type
+                .as_node_ref()
+                .add_issue(self.i_s, IssueKind::OnlyClassTypeApplication);
+            return Inferred::new_any_from_error();
+        }
+        compute_type_application!(
+            self,
+            slice_type,
+            from_alias_definition,
+            compute_type_get_item_on_alias(alias, slice_type)
+        )
+    }
+
+    pub(crate) fn compute_type_application_on_typing_class(
+        &self,
+        specific: Specific,
+        slice_type: SliceType,
+        from_alias_definition: bool,
+    ) -> Inferred {
+        match specific {
+            Specific::TypingGeneric | Specific::TypingProtocol => {
+                self.add_issue(
+                    slice_type.cst_node.index(),
+                    IssueKind::InvalidType("Invalid type application".to_string().into()),
+                );
+                Inferred::new_any_from_error()
+            }
+            Specific::TypingTuple => {
+                compute_type_application!(
+                    self,
+                    slice_type,
+                    from_alias_definition,
+                    compute_type_get_item_on_tuple(slice_type)
+                )
+            }
+            Specific::TypingCallable => {
+                compute_type_application!(
+                    self,
+                    slice_type,
+                    from_alias_definition,
+                    compute_type_get_item_on_callable(slice_type)
+                )
+            }
+            Specific::TypingUnion => {
+                compute_type_application!(
+                    self,
+                    slice_type,
+                    from_alias_definition,
+                    compute_type_get_item_on_union(slice_type)
+                )
+            }
+            Specific::TypingOptional => {
+                compute_type_application!(
+                    self,
+                    slice_type,
+                    from_alias_definition,
+                    compute_type_get_item_on_optional(slice_type)
+                )
+            }
+            Specific::TypingType | Specific::BuiltinsType => {
+                compute_type_application!(
+                    self,
+                    slice_type,
+                    from_alias_definition,
+                    compute_type_get_item_on_type(slice_type)
+                )
+            }
+            Specific::TypingLiteral => {
+                compute_type_application!(
+                    self,
+                    slice_type,
+                    from_alias_definition,
+                    compute_get_item_on_literal(slice_type)
+                )
+            }
+            Specific::TypingAnnotated => {
+                compute_type_application!(
+                    self,
+                    slice_type,
+                    from_alias_definition,
+                    compute_get_item_on_annotated(slice_type)
+                )
+            }
+            Specific::MypyExtensionsFlexibleAlias => {
+                compute_type_application!(
+                    self,
+                    slice_type,
+                    from_alias_definition,
+                    compute_get_item_on_flexible_alias(slice_type)
+                )
+            }
+            _ => unreachable!("{:?}", specific),
+        }
+    }
+}
