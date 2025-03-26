a3c4bd90d9d90f50f30498444d6ed098a7ecf5bf
Dave Halter <davidhalter88@gmail.com>
2025-03-26 18:40:17 +0100

    Move is_reexport_issue

--- crates/zuban_python/src/file/name_resolution.rs
+++ crates/zuban_python/src/file/name_resolution.rs
@@ -14,7 +14,6 @@ use crate::{
     inferred::Inferred,
     node_ref::NodeRef,
     type_::{LookupResult, Type},
-    type_helpers::{is_private_import, is_reexport_issue},
     utils::AlreadySeen,
 };
 
@@ -1067,4 +1066,25 @@ impl<'db, 'file, 'i_s> NameResolution<'db, 'file, 'i_s> {
 
 fn is_valid_builtins_export(name: &str) -> bool {
     !name.starts_with('_') || name.starts_with("__") && name.ends_with("__")
+}
+
+fn is_reexport_issue(db: &Database, name_ref: NodeRef) -> bool {
+    if !name_ref.file.is_stub() && !name_ref.file.flags(db).no_implicit_reexport {
+        return false;
+    }
+    if let Some(dunder_all) = name_ref.file.maybe_dunder_all(db) {
+        debug_assert!(name_ref.maybe_name().is_some());
+        let name = name_ref.as_code();
+        if dunder_all.iter().any(|d| d.as_str(db) == name) || name == "__all__" {
+            // Name was exported in __all__
+            return false;
+        }
+    }
+    is_private_import(name_ref)
+}
+
+fn is_private_import(name_ref: NodeRef) -> bool {
+    name_ref
+        .maybe_import_of_name_in_symbol_table()
+        .is_some_and(|i| !i.is_stub_reexport())
 }
--- crates/zuban_python/src/type_helpers/module.rs
+++ crates/zuban_python/src/type_helpers/module.rs
@@ -4,7 +4,6 @@ use crate::{
     file::PythonFile,
     imports::{namespace_import, ImportResult},
     inferred::Inferred,
-    node_ref::NodeRef,
     type_::{AnyCause, LookupResult, Namespace, Type},
 };
 
@@ -25,25 +24,4 @@ pub fn lookup_in_namespace(
             LookupResult::None
         }
     }
-}
-
-pub fn is_reexport_issue(db: &Database, name_ref: NodeRef) -> bool {
-    if !name_ref.file.is_stub() && !name_ref.file.flags(db).no_implicit_reexport {
-        return false;
-    }
-    if let Some(dunder_all) = name_ref.file.maybe_dunder_all(db) {
-        debug_assert!(name_ref.maybe_name().is_some());
-        let name = name_ref.as_code();
-        if dunder_all.iter().any(|d| d.as_str(db) == name) || name == "__all__" {
-            // Name was exported in __all__
-            return false;
-        }
-    }
-    is_private_import(name_ref)
-}
-
-pub fn is_private_import(name_ref: NodeRef) -> bool {
-    name_ref
-        .maybe_import_of_name_in_symbol_table()
-        .is_some_and(|i| !i.is_stub_reexport())
 }
