f93f6362b40d7d7d80f417942ffbd23ffd62c2a0
Dave Halter <davidhalter88@gmail.com>
2025-04-30 14:01:58 +0200

    Pin the version of cargo-release

--- scripts/release.sh
+++ scripts/release.sh
@@ -6,5 +6,5 @@ cd "$(dirname "$0")"
 # path is the default level
 LEVEL="${1:-patch}"
 
-cargo install cargo-release
+cargo install cargo-release --version 0.25.17
 cargo release --workspace --tag-prefix '' --no-publish --execute "$LEVEL"
