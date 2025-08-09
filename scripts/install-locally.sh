ba93452b1883d9252d10f92d7187bc94ea95b6b4
Dave Halter <davidhalter88@gmail.com>
2025-08-09 13:54:34 +0200

    Add the option to create a dev build in install-locally.sh

--- scripts/install-locally.sh
+++ scripts/install-locally.sh
@@ -5,6 +5,13 @@ cd "$(dirname "$0")"
 rm -f ../target/wheels/*
 cd ../deploy/pypi/zuban
 
-maturin build --release
+if [ "$1" == "dev" ]; then
+    echo "Creating a dev build"
+    maturin build
+else
+    echo "Creating a release build"
+    maturin build --release
+fi
+
 
 pip install ../../../target/wheels/zuban-*.whl --force-reinstall --break-system-packages
