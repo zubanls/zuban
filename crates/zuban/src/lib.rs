aee53132ac1c224115359819f5835d39c79bf910
Dave Halter <davidhalter88@gmail.com>
2025-08-27 21:45:16 +0200

    Make --version work on zmypy/zubanls

--- crates/zuban/src/lib.rs
+++ crates/zuban/src/lib.rs
@@ -1,13 +1,18 @@
 use std::env;
 use std::process::{exit, Command};
 
-pub fn execute_zuban_executable_with_subcommand(subcommand: &str) {
+pub fn execute_zuban_executable_with_subcommand(mut subcommand: &str) {
     // Collect args except the binary name
-    let args: Vec<String> = env::args().skip(1).collect();
+    let mut args: Vec<String> = env::args().skip(1).collect();
 
     let mut zuban_path = env::current_exe().expect("failed to get current exe path");
     // replace "zubanls"/"zmypy" with "zuban"
     zuban_path.set_file_name("zuban");
+
+    if args.len() == 1 && (args[0] == "--version" || args[0] == "-V") {
+        args.clear();
+        subcommand = "--version"
+    }
 
     // Run "./zuban server <args...>"
     let status = Command::new(zuban_path)
