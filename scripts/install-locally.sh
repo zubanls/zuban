f77003461d0c6fbf14c26e7f0e211a9ba834871c
Dave Halter <davidhalter88@gmail.com>
2025-08-31 15:09:26 +0200

    Installing locally now takes different flags

--- scripts/install-locally.sh
+++ scripts/install-locally.sh
@@ -5,13 +5,12 @@ cd "$(dirname "$0")"
 rm -f ../target/wheels/*
 cd ../deploy/pypi/zuban
 
-if [ "$1" == "dev" ]; then
-    echo "Creating a dev build"
-    maturin build
+if [ $# -eq 0 ]; then
+    echo "Creating a dev build (for release builds pass --release)"
 else
-    echo "Creating a release build"
-    maturin build --release
+    echo "Creating a build with maturin build ${@:1}"
 fi
+maturin build "${@:1}"
 
 
 pip install ../../../target/wheels/zuban-*.whl --force-reinstall --break-system-packages
