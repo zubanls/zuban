f062246e7572639cfade771626f7d75b2a217d95
marcelarie <googlillo@gmail.com>
2025-09-04 12:32:08 +0200

    setup flake.nix for package installation via nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = pkgs.rustPlatform.buildRustPackage {
+      pname = "zuban";
+      version = "0.0.21";
+      src = pkgs.lib.cleanSourceWith {
+        src = ./.;
+        filter = name: type: !(pkgs.lib.hasSuffix ".git" name);
+      };
+
+      preBuild = ''
+        export TYPESHED_HOME=${pkgs.fetchFromGitHub {
+          owner = "python";
+          repo = "typeshed";
+          rev = "1b267b25f2c726291e4e6627b7567f2c8dc04b60";
+          sha256 = "sha256-SQJypewNYLfNSIc+Myd9l+nypdMdOx+Fyymmg/YpFW4=";
+        }}
+        cp -r $TYPESHED_HOME typeshed
+      '';
+      cargoLock = {
+        lockFile = ./Cargo.lock;
+        outputHashes = {
+          "rust-ini-0.21.1" = "sha256-0NRWwxSdMjnu/T2JW1BNUYNLJdtqk5J5WYs7VXbltRs=";
+        };
+      };
+      doCheck = false;
+    };
+  };
+}
+
