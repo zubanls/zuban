5f3b6990a72fee27b6ba66d4f841658f844421ce
marcelarie <googlillo@gmail.com>
2025-09-04 15:13:50 +0200

    fix(nix): correct typeshed library installation path

--- flake.nix
+++ flake.nix
@@ -31,13 +31,13 @@
       doCheck = false;
       
       postInstall = ''
-        mkdir -p $out/lib
+        mkdir -p $out/lib/python3.13/site-packages/zuban
         cp -r ${pkgs.fetchFromGitHub {
           owner = "python";
           repo = "typeshed";
           rev = "1b267b25f2c726291e4e6627b7567f2c8dc04b60";
           sha256 = "sha256-SQJypewNYLfNSIc+Myd9l+nypdMdOx+Fyymmg/YpFW4=";
-        }}/* $out/lib/
+        }} $out/lib/python3.13/site-packages/zuban/typeshed
       '';
     };
   };
