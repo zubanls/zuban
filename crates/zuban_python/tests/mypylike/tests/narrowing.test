zubanls/zuban

commit fa69d1adea08eb70efccf276071d84ac4add3d4c

Rewrite and fix walrus narrowing, fixes #24

crates/zuban_python/src/file/flow_analysis.rs
@@ -25,7 +25,7 @@ use crate::{
     },
     debug,
     diagnostics::IssueKind,
-    file::{ClassNodeRef, OtherDefinitionIterator},
+    file::{ClassNodeRef, OtherDefinitionIterator, first_defined_name_of_multi_def},
     getitem::SliceType,
     inference_state::InferenceState,
     inferred::{Inferred, UnionValue, add_attribute_error},
@@ -149,7 +149,7 @@ struct Entry {
 
 impl Entry {
     fn new(key: FlowKey, type_: Type) -> Self {
-        Entry {
+        Self {
             key,
             type_: EntryKind::Type(type_),
             modifies_ancestors: false,
@@ -324,10 +324,6 @@ impl Frame {
             }
         }
         self.entries.push(entry)
-    }
-
-    fn add_entry_from_type(&mut self, i_s: &InferenceState, key: FlowKey, type_: Type) {
-        self.add_entry(i_s, Entry::new(key, type_))
     }
 
     fn from_type_without_entry(t: Type) -> Self {
@@ -2835,36 +2831,54 @@ impl Inference<'_, '_, '_> {
                     } else {
                         self.find_guards_in_expr(expr)
                     };
-                FLOW_ANALYSIS.with(|fa| {
-                    let (walrus_frame, inf) =
-                        fa.with_frame_and_result(Frame::new_conditional(), || {
+                let is_first =
+                    first_defined_name_of_multi_def(self.file, name_def.name_index()).is_none();
+                let inf =
                             // This can happen in weird closures. I'm not currently sure how we should
                             // best handle that, see avoid_walrus_crash_when_variable_is_used_in_closure
                             if self.point(name_def.index()).calculated() {
-                                return inf;
-                            }
-                            self.save_walrus(name_def, inf.into_inferred(self.i_s))
-                                .into()
-                        });
-                    if let Some((walrus_truthy, walrus_falsey)) =
-                        split_truthy_and_falsey(self.i_s, &inf)
-                    {
-                        debug!(
-                            "Narrowed {} to true: {} and false: {}",
-                            named_expr.as_code(),
-                            walrus_truthy.format_short(self.i_s.db),
-                            walrus_falsey.format_short(self.i_s.db)
-                        );
-                        let key = self.key_from_name_def(name_def);
-                        truthy.add_entry_from_type(self.i_s, key.clone(), walrus_truthy);
-                        falsey.add_entry_from_type(self.i_s, key, walrus_falsey);
-                    }
-                    for entry in walrus_frame.entries {
-                        truthy.overwrite_entry(self.i_s.db, entry.clone());
-                        falsey.overwrite_entry(self.i_s.db, entry);
-                    }
-                    (inf, truthy, falsey)
-                })
+                                inf
+                            } else if is_first {
+                                self.save_walrus(name_def, inf.into_inferred(self.i_s))
+                                    .into()
+                            } else {
+                                inf
+                            };
+                if let Some((walrus_truthy, walrus_falsey)) =
+                    split_truthy_and_falsey(self.i_s, &inf)
+                {
+                    debug!(
+                        "Narrowed {} to true: {} and false: {}",
+                        named_expr.as_code(),
+                        walrus_truthy.format_short(self.i_s.db),
+                        walrus_falsey.format_short(self.i_s.db)
+                    );
+                    let key = self.key_from_name_def(name_def);
+                    truthy.add_entry(
+                        self.i_s,
+                        Entry {
+                            modifies_ancestors: true,
+                            ..Entry::new(key.clone(), walrus_truthy)
+                        },
+                    );
+                    falsey.add_entry(
+                        self.i_s,
+                        Entry {
+                            modifies_ancestors: true,
+                            ..Entry::new(key, walrus_falsey)
+                        },
+                    );
+                } else if !is_first {
+                    let t = inf.clone().into_inferred(self.i_s).as_type(self.i_s);
+                    let new_entry = Entry {
+                        modifies_ancestors: true,
+                        widens: true,
+                        ..Entry::new(self.key_from_name_def(name_def), t)
+                    };
+                    truthy.overwrite_entry(self.i_s.db, new_entry.clone());
+                    falsey.overwrite_entry(self.i_s.db, new_entry);
+                }
+                (inf, truthy, falsey)
             }
         }
     }
@@ -3955,6 +3969,7 @@ impl Inference<'_, '_, '_> {
     }
 }
 
+#[derive(Clone)]
 enum TruthyInferred {
     Simple {
         inf: Inferred,

crates/zuban_python/tests/mypylike/tests/narrowing.test
@@ -490,6 +490,35 @@ def bar() -> None:
         reveal_type(x)  # N: Revealed type is "__main__.B"
     reveal_type(x)  # N: Revealed type is "__main__.B"
 
+[case walrus_narrowing_with_initial_definition_in_if]
+d = {'hello': 'hi'}
+
+if x:= d.get('hello'):
+    reveal_type(x)  # N: Revealed type is "str"
+elif x:= d.get('world'):
+    reveal_type(x)  # N: Revealed type is "str"
+else:
+    reveal_type(x)  # N: Revealed type is "Literal[''] | None"
+
+[case walrus_narrowing_with_initial_definition_in_class_body]
+class C:
+    x: int | None
+
+    def func(self) -> None:
+        if x := self.x:
+            reveal_type(x) # N: Revealed type is "int"
+        else:
+            reveal_type(x) # N: Revealed type is "Literal[0] | None"
+
+[case walrus_narrowing_with_unreachable_if]
+# flags: --warn-unreachable
+from typing import Literal
+def f(x: Literal[0] | None):
+    if y := x:
+        reveal_type(y)  # E: Statement is unreachable
+    else:
+        reveal_type(y)  # N: Revealed type is "Literal[0] | None"
+
 [case multi_while_break_widening]
 class A: ...
 class B: ...

commit 0debc53a253a65f9ab6d66920962c7d720655f97

Fix walrus redefinitions that was unintentially broken in fa69d1a, see also #24

crates/zuban_python/src/file/flow_analysis.rs
@@ -25,7 +25,7 @@ use crate::{
     },
     debug,
     diagnostics::IssueKind,
-    file::{ClassNodeRef, OtherDefinitionIterator, first_defined_name_of_multi_def},
+    file::{ClassNodeRef, OtherDefinitionIterator},
     getitem::SliceType,
     inference_state::InferenceState,
     inferred::{Inferred, UnionValue, add_attribute_error},
@@ -149,7 +149,7 @@ struct Entry {
 
 impl Entry {
     fn new(key: FlowKey, type_: Type) -> Self {
-        Self {
+        Entry {
             key,
             type_: EntryKind::Type(type_),
             modifies_ancestors: false,
@@ -2831,54 +2831,65 @@ impl Inference<'_, '_, '_> {
                     } else {
                         self.find_guards_in_expr(expr)
                     };
-                let is_first =
-                    first_defined_name_of_multi_def(self.file, name_def.name_index()).is_none();
-                let inf =
+                FLOW_ANALYSIS.with(|fa| {
+                    let (walrus_frame, (narrowing_result, inf)) =
+                        fa.with_frame_and_result(Frame::new_conditional(), || {
                             // This can happen in weird closures. I'm not currently sure how we should
                             // best handle that, see avoid_walrus_crash_when_variable_is_used_in_closure
                             if self.point(name_def.index()).calculated() {
-                                inf
-                            } else if is_first {
-                                self.save_walrus(name_def, inf.into_inferred(self.i_s))
-                                    .into()
-                            } else {
-                                inf
-                            };
-                if let Some((walrus_truthy, walrus_falsey)) =
-                    split_truthy_and_falsey(self.i_s, &inf)
-                {
-                    debug!(
-                        "Narrowed {} to true: {} and false: {}",
-                        named_expr.as_code(),
-                        walrus_truthy.format_short(self.i_s.db),
-                        walrus_falsey.format_short(self.i_s.db)
-                    );
+                                return (None, inf);
+                            }
+                            let (narrowing_result, inf) =
+                                self.save_walrus(name_def, inf.into_inferred(self.i_s));
+                            (narrowing_result, inf.into())
+                        });
+
                     let key = self.key_from_name_def(name_def);
-                    truthy.add_entry(
-                        self.i_s,
-                        Entry {
-                            modifies_ancestors: true,
-                            ..Entry::new(key.clone(), walrus_truthy)
-                        },
-                    );
-                    falsey.add_entry(
-                        self.i_s,
-                        Entry {
-                            modifies_ancestors: true,
-                            ..Entry::new(key, walrus_falsey)
-                        },
-                    );
-                } else if !is_first {
-                    let t = inf.clone().into_inferred(self.i_s).as_type(self.i_s);
-                    let new_entry = Entry {
-                        modifies_ancestors: true,
-                        widens: true,
-                        ..Entry::new(self.key_from_name_def(name_def), t)
-                    };
-                    truthy.overwrite_entry(self.i_s.db, new_entry.clone());
-                    falsey.overwrite_entry(self.i_s.db, new_entry);
-                }
-                (inf, truthy, falsey)
+                    if matches!(
+                        narrowing_result,
+                        Some(RedefinitionResult::TypeMismatch(true))
+                    ) {
+                        return (
+                            Inferred::new_any_from_error().into(),
+                            Frame::new_conditional(),
+                            Frame::new_conditional(),
+                        );
+                    } else if let Some((walrus_truthy, walrus_falsey)) =
+                        split_truthy_and_falsey(self.i_s, &inf)
+                    {
+                        debug!(
+                            "Narrowed {} to true: {} and false: {}",
+                            named_expr.as_code(),
+                            walrus_truthy.format_short(self.i_s.db),
+                            walrus_falsey.format_short(self.i_s.db)
+                        );
+                        let widens = walrus_frame
+                            .entries
+                            .into_iter()
+                            .any(|e| e.key.equals(self.i_s.db, &key) && e.widens);
+                        truthy.overwrite_entry(
+                            self.i_s.db,
+                            Entry {
+                                widens,
+                                modifies_ancestors: true,
+                                ..Entry::new(key.clone(), walrus_truthy)
+                            },
+                        );
+                        falsey.overwrite_entry(
+                            self.i_s.db,
+                            Entry {
+                                widens,
+                                ..Entry::new(key, walrus_falsey)
+                            },
+                        );
+                    } else {
+                        for entry in walrus_frame.entries {
+                            truthy.overwrite_entry(self.i_s.db, entry.clone());
+                            falsey.overwrite_entry(self.i_s.db, entry);
+                        }
+                    }
+                    (inf, truthy, falsey)
+                })
             }
         }
     }
@@ -3969,7 +3980,6 @@ impl Inference<'_, '_, '_> {
     }
 }
 
-#[derive(Clone)]
 enum TruthyInferred {
     Simple {
         inf: Inferred,
@@ -4029,6 +4039,7 @@ impl From<Inferred> for TruthyInferred {
     }
 }
 
+#[derive(Copy, Clone)]
 pub(crate) enum RedefinitionResult {
     RedefinitionAllowed,
     TypeMismatch(bool),

crates/zuban_python/src/file/inference.rs
@@ -179,7 +179,7 @@ impl<'db, 'file> Inference<'db, 'file, '_> {
                         .save_redirect(self.i_s, self.file, name_def.index());
                 }
             },
-        )
+        );
     }
 
     fn inferred_context_for_simple_assignment(
@@ -1056,7 +1056,7 @@ impl<'db, 'file> Inference<'db, 'file, '_> {
         value: &Inferred,
         assign_kind: AssignKind,
         save: impl FnOnce(NodeIndex, &Inferred),
-    ) {
+    ) -> Option<RedefinitionResult> {
         debug!(
             "Assign to name {} ({}:#{}): {}",
             name_def.as_code(),
@@ -1098,10 +1098,11 @@ impl<'db, 'file> Inference<'db, 'file, '_> {
                         &ancestor_inf,
                         save,
                     );
-                    return;
+                    return None; // TODO?
                 }
             }
         }
+        let result = Cell::new(None);
         self.assign_to_name_def_or_self_name_def(
             name_def,
             from,
@@ -1115,23 +1116,27 @@ impl<'db, 'file> Inference<'db, 'file, '_> {
             |first_name_link, declaration_t| {
                 let current_t = value.as_cow_type(i_s);
                 self.narrow_or_widen_name_target(first_name_link, declaration_t, &current_t, || {
-                    if self.flags().allow_redefinition
+                    let r = if self.flags().allow_redefinition
                         && NodeRef::from_link(self.i_s.db, first_name_link)
                             .point()
                             .can_be_redefined()
                     {
-                        return RedefinitionResult::RedefinitionAllowed;
-                    }
-                    RedefinitionResult::TypeMismatch(self.check_assignment_type(
-                        value,
-                        declaration_t,
-                        from,
-                        None,
-                        assign_kind,
-                    ))
+                        RedefinitionResult::RedefinitionAllowed
+                    } else {
+                        RedefinitionResult::TypeMismatch(self.check_assignment_type(
+                            value,
+                            declaration_t,
+                            from,
+                            None,
+                            assign_kind,
+                        ))
+                    };
+                    result.set(Some(r));
+                    r
                 })
             },
-        )
+        );
+        result.into_inner()
     }
 
     fn check_assignment_type(
@@ -1816,7 +1821,7 @@ impl<'db, 'file> Inference<'db, 'file, '_> {
         let i_s = self.i_s;
         match target {
             Target::Name(name_def) => {
-                self.assign_to_name_def(name_def, from, value, assign_kind, save)
+                self.assign_to_name_def(name_def, from, value, assign_kind, save);
             }
             Target::NameExpression(primary_target, name_def) => {
                 if self.is_self(primary_target.first()) {
@@ -2233,7 +2238,7 @@ impl<'db, 'file> Inference<'db, 'file, '_> {
             self.infer_expression(expr)
         };
 
-        self.save_walrus(name_def, inf)
+        self.save_walrus(name_def, inf).1
     }
 
     pub(crate) fn check_for_redefinition(
@@ -2304,11 +2309,21 @@ impl<'db, 'file> Inference<'db, 'file, '_> {
             .and_then(|inf| inf.maybe_saved_node_ref(self.i_s.db))
     }
 
-    pub fn save_walrus(&self, name_def: NameDef, inf: Inferred) -> Inferred {
+    pub fn save_walrus(
+        &self,
+        name_def: NameDef,
+        inf: Inferred,
+    ) -> (Option<RedefinitionResult>, Inferred) {
         let from = NodeRef::new(self.file, name_def.index());
         let inf = inf.avoid_implicit_literal(self.i_s);
-        self.assign_to_name_def_simple(name_def, from, &inf, AssignKind::Normal);
-        self.check_point_cache(name_def.index()).unwrap_or(inf)
+        let narrowing_result =
+            self.assign_to_name_def(name_def, from, &inf, AssignKind::Normal, |index, value| {
+                value.clone().save_redirect(self.i_s, self.file, index);
+            });
+        (
+            narrowing_result,
+            self.check_point_cache(name_def.index()).unwrap_or(inf),
+        )
     }
 
     pub fn infer_expression(&self, expr: Expression) -> Inferred {

crates/zuban_python/tests/mypylike/tests/narrowing.test
@@ -510,6 +510,18 @@ class C:
         else:
             reveal_type(x) # N: Revealed type is "Literal[0] | None"
 
+[case walrus_narrowing_with_initial_definition_outside_if_wrong_type]
+d = {'hello': 'hi'}
+
+x: str | None
+
+if x:= d.get('hello'):
+    reveal_type(x)  # N: Revealed type is "str"
+elif x:= 1:  # E: Incompatible types in assignment (expression has type "int", variable has type "str | None")
+    reveal_type(x)  # N: Revealed type is "Literal[''] | None"
+else:
+    reveal_type(x)  # N: Revealed type is "Literal[''] | None"
+
 [case walrus_narrowing_with_unreachable_if]
 # flags: --warn-unreachable
 from typing import Literal