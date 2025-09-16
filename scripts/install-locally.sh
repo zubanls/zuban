5501e72041462d3ea2b13c1ccba1667ddab57b5a
Artem Golubin <me@rushter.com>
2025-09-16 09:13:10 +0400

    Fix fresh dev install

--- scripts/install-locally.sh
+++ scripts/install-locally.sh
@@ -10,6 +10,10 @@ if [ $# -eq 0 ]; then
 else
     echo "Creating a build with maturin build ${@:1}"
 fi
+
+if [ ! -f licenses.html ]; then
+    bash pre-maturin-build.sh
+fi
 maturin build "${@:1}"
 
 
