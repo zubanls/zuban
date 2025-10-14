fa0fe7780389becda4b2b8975b7b7925ac265c75
Dave Halter <davidhalter88@gmail.com>
2025-10-14 18:50:31 +0200

    Way better call signature positions

--- crates/parsa_python_cst/src/signatures.rs
+++ crates/parsa_python_cst/src/signatures.rs
@@ -2,8 +2,8 @@ use parsa_python::{
     CodeIndex, NodeIndex,
     NonterminalType::*,
     PyNode,
-    PyNodeType::{ErrorNonterminal, Nonterminal},
-    SiblingIterator,
+    PyNodeType::{ErrorNonterminal, Nonterminal, Terminal},
+    SiblingIterator, TerminalType,
 };
 
 use crate::{
@@ -137,8 +137,16 @@ impl<'db> Iterator for SignatureArgsIterator<'db> {
                 if arg.is_type(Nonterminal(kwargs)) || arg.is_type(ErrorNonterminal(kwargs)) {
                     *args = arg.iter_children();
                     self.next()
-                } else if arg.is_type(Nonterminal(kwarg)) || arg.is_type(ErrorNonterminal(kwarg)) {
+                } else if arg.is_type(Nonterminal(kwarg)) {
                     Some(SignatureArg::Keyword(Name::new(arg.nth_child(0))))
+                } else if arg.is_type(ErrorNonterminal(kwarg)) {
+                    let name = arg.nth_child(0);
+                    debug_assert!(matches!(name.type_(), Terminal(TerminalType::Name)));
+                    if name.next_leaf().is_some_and(|leaf| leaf.as_code() == "=") {
+                        Some(SignatureArg::Keyword(Name::new(name)))
+                    } else {
+                        Some(SignatureArg::PositionalOrKeywordName(name.as_code()))
+                    }
                 } else if arg.is_type(Nonterminal(starred_expression))
                     || arg.is_type(ErrorNonterminal(starred_expression))
                 {
@@ -167,17 +175,29 @@ pub enum SignatureBase<'db> {
 
 #[derive(Debug, Clone)]
 pub struct ErrorStmtSignaturePart<'db> {
-    stmt_: Option<PyNode<'db>>,
-    //iterator_node: PyNode<'db>,
-    last_node: NodeIndex,
+    stmt_: PyNode<'db>,
+    inner_stmt_iterator: ErrorInnerStmtSignaturePart<'db>,
 }
 
 impl<'db> ErrorStmtSignaturePart<'db> {
     fn new(stmt_: PyNode<'db>, last_node: NodeIndex) -> Self {
+        let first_child = stmt_.nth_child(0);
+        dbg!(first_child);
+        let inner_stmt_iterator = if first_child.is_type(ErrorNonterminal(simple_stmts)) {
+            dbg!(first_child.nth_child(0).nth_child(0)).iter_children()
+        } else {
+            stmt_.iter_children()
+        };
+        for x in inner_stmt_iterator.clone() {
+            dbg!(x);
+        }
+
         Self {
-            stmt_: Some(stmt_),
-            //iterator_node,
-            last_node,
+            stmt_: stmt_,
+            inner_stmt_iterator: ErrorInnerStmtSignaturePart {
+                inner_stmt_iterator,
+                last_node,
+            },
         }
     }
 }
@@ -186,15 +206,46 @@ impl<'db> Iterator for ErrorStmtSignaturePart<'db> {
     type Item = SignatureArg<'db>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let stmt_ = self.stmt_?;
-        if stmt_.index > self.last_node {
+        for inner in &mut self.inner_stmt_iterator {
+            dbg!(inner);
+            if inner.as_code() == "," {
+                if let Some(name) = inner.next_leaf() {
+                    dbg!(name);
+                    if name.is_type(Terminal(TerminalType::Name)) {
+                        dbg!(name.next_leaf());
+                        if let Some(next) = name.next_leaf()
+                            && next.as_code() == "="
+                        {
+                            return Some(SignatureArg::Keyword(Name::new(name)));
+                        } else {
+                            return Some(SignatureArg::PositionalOrKeywordName(name.as_code()));
+                        }
+                    }
+                }
+                return Some(SignatureArg::PositionalOrEmptyAfterComma);
+            }
+        }
+        let new = self.stmt_.next_sibling()?;
+        if new.index > self.inner_stmt_iterator.last_node {
             return None;
         }
-        let mut result = None;
-        if stmt_.as_code() == "," {
-            result = Some(SignatureArg::PositionalOrEmptyAfterComma)
-        }
-        self.stmt_ = stmt_.next_sibling();
-        result
+        *self = Self::new(new, self.inner_stmt_iterator.last_node);
+        self.next()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ErrorInnerStmtSignaturePart<'db> {
+    inner_stmt_iterator: SiblingIterator<'db>,
+    last_node: NodeIndex,
+}
+
+impl<'db> Iterator for ErrorInnerStmtSignaturePart<'db> {
+    type Item = PyNode<'db>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.inner_stmt_iterator
+            .next()
+            .filter(|node| node.index <= self.last_node)
     }
 }
