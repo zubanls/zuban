d4e043ebd147025e6e76d436713cd7325eb2aca3
Dave Halter <davidhalter88@gmail.com>
2025-11-11 02:08:55 +0100

    Remove the typeshed symbols crates again

--- crates/typeshed_symbols_generator/src/main.rs
+++
@@ -1,7 +0,0 @@
-use config::ProjectOptions;
-use zuban_python::{Mode, Project};
-
-fn main() {
-    let project = Project::without_watcher(ProjectOptions::default(), Mode::LanguageServer);
-    project;
-}
