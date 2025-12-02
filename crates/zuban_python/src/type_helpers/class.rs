d9a14e1bc3677ae73f241387c3219023f2fa19ed
Dave Halter <davidhalter88@gmail.com>
2025-12-02 16:18:44 +0100

    Remove a dbg

--- crates/zuban_python/src/type_helpers/class.rs
+++ crates/zuban_python/src/type_helpers/class.rs
@@ -1696,7 +1696,7 @@ impl<'db: 'a, 'a> Class<'a> {
                             return None;
                         }
                         if let Some(name) = redirected_to.maybe_name()
-                            && let Some(name_def) = dbg!(name.name_def())
+                            && let Some(name_def) = name.name_def()
                             && let Some(func) = name_def.parent_function_of_param()
                         {
                             let parent_scope =
