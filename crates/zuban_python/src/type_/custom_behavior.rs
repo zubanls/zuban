473e2470b229ef0dd45eeefe10063df8c5114fc4
Dave Halter <davidhalter88@gmail.com>
2026-01-08 00:40:19 +0100

    Move CustomBehavior

---
+++ crates/zuban_python/src/type_/custom_behavior.rs
@@ -0,0 +1,82 @@
+use std::sync::Arc;
+
+use crate::{
+    arguments::Args,
+    inference_state::InferenceState,
+    inferred::Inferred,
+    matching::{OnTypeError, ResultContext},
+    type_::Type,
+};
+
+type CustomBehaviorCallback = for<'db> fn(
+    i_s: &InferenceState<'db, '_>,
+    args: &dyn Args<'db>,
+    result_context: &mut ResultContext,
+    on_type_error: OnTypeError,
+    bound: Option<&Type>,
+) -> Inferred;
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+pub(crate) enum CustomBehaviorKind {
+    Function,
+    Method { bound: Option<Arc<Type>> },
+}
+
+#[allow(clippy::derived_hash_with_manual_eq)]
+#[derive(Debug, Eq, Hash, Clone)]
+pub(crate) struct CustomBehavior {
+    callback: CustomBehaviorCallback,
+    kind: CustomBehaviorKind,
+}
+
+impl PartialEq for CustomBehavior {
+    fn eq(&self, _: &Self) -> bool {
+        // For now these simply don't match, I'm not sure it's important that they ever do.
+        // PartialEq is implemented more as a "quickly check if the types are exactly the same,
+        // otherwise simply fall back to normal type matching behavior, which is much more complex
+        // anyways".
+        false
+    }
+}
+
+impl CustomBehavior {
+    pub(crate) fn new_function(callback: CustomBehaviorCallback) -> Self {
+        Self {
+            callback,
+            kind: CustomBehaviorKind::Function,
+        }
+    }
+
+    pub(crate) fn new_method(callback: CustomBehaviorCallback, bound: Option<Arc<Type>>) -> Self {
+        Self {
+            callback,
+            kind: CustomBehaviorKind::Method { bound },
+        }
+    }
+
+    pub fn bind(&self, bound: Arc<Type>) -> Self {
+        Self {
+            callback: self.callback,
+            kind: match self.kind {
+                CustomBehaviorKind::Method { bound: None } => {
+                    CustomBehaviorKind::Method { bound: Some(bound) }
+                }
+                _ => self.kind.clone(),
+            },
+        }
+    }
+
+    pub(crate) fn execute<'db>(
+        &self,
+        i_s: &InferenceState<'db, '_>,
+        args: &dyn Args<'db>,
+        result_context: &mut ResultContext,
+        on_type_error: OnTypeError,
+    ) -> Inferred {
+        let bound = match &self.kind {
+            CustomBehaviorKind::Function => None,
+            CustomBehaviorKind::Method { bound } => bound.as_deref(),
+        };
+        (self.callback)(i_s, args, result_context, on_type_error, bound)
+    }
+}
--- crates/zuban_python/src/type_/mod.rs
+++ crates/zuban_python/src/type_/mod.rs
@@ -1,6 +1,7 @@
 mod callable;
 mod common_base_type;
 mod common_sub_type;
+mod custom_behavior;
 mod dataclass;
 mod enum_;
 mod intersection;
@@ -36,6 +37,7 @@ pub(crate) use self::{
         add_param_spec_to_params, format_callable_params, format_params_as_param_spec,
         merge_class_type_vars,
     },
+    custom_behavior::{CustomBehavior, CustomBehaviorKind},
     dataclass::{
         Dataclass, DataclassOptions, DataclassTransformObj, dataclass_converter_fields_lookup,
         dataclass_init_func, dataclass_initialize, dataclass_post_init_func, dataclasses_replace,
@@ -2129,79 +2131,6 @@ impl Literal {
             question_mark
         )
         .into()
-    }
-}
-
-type CustomBehaviorCallback = for<'db> fn(
-    i_s: &InferenceState<'db, '_>,
-    args: &dyn Args<'db>,
-    result_context: &mut ResultContext,
-    on_type_error: OnTypeError,
-    bound: Option<&Type>,
-) -> Inferred;
-
-#[derive(Debug, PartialEq, Eq, Clone, Hash)]
-pub(crate) enum CustomBehaviorKind {
-    Function,
-    Method { bound: Option<Arc<Type>> },
-}
-
-#[allow(clippy::derived_hash_with_manual_eq)]
-#[derive(Debug, Eq, Hash, Clone)]
-pub(crate) struct CustomBehavior {
-    callback: CustomBehaviorCallback,
-    kind: CustomBehaviorKind,
-}
-
-impl PartialEq for CustomBehavior {
-    fn eq(&self, _: &Self) -> bool {
-        // For now these simply don't match, I'm not sure it's important that they ever do.
-        // PartialEq is implemented more as a "quickly check if the types are exactly the same,
-        // otherwise simply fall back to normal type matching behavior, which is much more complex
-        // anyways".
-        false
-    }
-}
-
-impl CustomBehavior {
-    pub(crate) fn new_function(callback: CustomBehaviorCallback) -> Self {
-        Self {
-            callback,
-            kind: CustomBehaviorKind::Function,
-        }
-    }
-
-    pub(crate) fn new_method(callback: CustomBehaviorCallback, bound: Option<Arc<Type>>) -> Self {
-        Self {
-            callback,
-            kind: CustomBehaviorKind::Method { bound },
-        }
-    }
-
-    pub fn bind(&self, bound: Arc<Type>) -> Self {
-        Self {
-            callback: self.callback,
-            kind: match self.kind {
-                CustomBehaviorKind::Method { bound: None } => {
-                    CustomBehaviorKind::Method { bound: Some(bound) }
-                }
-                _ => self.kind.clone(),
-            },
-        }
-    }
-
-    pub(crate) fn execute<'db>(
-        &self,
-        i_s: &InferenceState<'db, '_>,
-        args: &dyn Args<'db>,
-        result_context: &mut ResultContext,
-        on_type_error: OnTypeError,
-    ) -> Inferred {
-        let bound = match &self.kind {
-            CustomBehaviorKind::Function => None,
-            CustomBehaviorKind::Method { bound } => bound.as_deref(),
-        };
-        (self.callback)(i_s, args, result_context, on_type_error, bound)
     }
 }
 
