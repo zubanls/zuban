dfce52e8cc3b5990f13fee5f71677725fd2b2b5f
Dave Halter <davidhalter88@gmail.com>
2026-04-25 11:45:47 +0200

    Pin cargo-about to the latest version

--- deploy/pypi/zuban/pre-maturin-build.sh
+++ deploy/pypi/zuban/pre-maturin-build.sh
@@ -7,5 +7,5 @@ BASE_DIR="../../.."
 WORKSPACE_TOML="$BASE_DIR/Cargo.toml"
 
 # Use --debug to speed up compilation
-cargo install --locked --debug cargo-about
+cargo install --features cli --locked --debug cargo-about --version 0.9.0
 cargo about generate -o licenses.html about.hbs --fail --manifest-path "$WORKSPACE_TOML"
