c1691c59b2d49d9c6aa4b319221ef3a2447413c5
Dave Halter <davidhalter88@gmail.com>
2026-05-21 15:39:05 +0200

    Add multi file tests

---
+++ crates/zuban_python/tests/jedilike/tests/multi_file1.py
@@ -0,0 +1,19 @@
+from multi_file2 import recursive_func2, in_multi_file2_func
+
+class Container:
+    def __init__(self, x):
+        self.x = x
+
+def recursive_func1(x):
+    #?
+    return recursive_func2(x)
+
+#?
+recursive_func1()
+#?
+recursive_func1(1)
+
+#? int()
+in_multi_file2_func(1)
+#? int()
+in_multi_file2_func(Container(1)).x
---
+++ crates/zuban_python/tests/jedilike/tests/multi_file2.py
@@ -0,0 +1,8 @@
+from multi_file1 import recursive_func1
+
+def recursive_func2(x):
+    #?
+    return recursive_func1(x)
+
+def in_multi_file2_func(x):
+    return x
