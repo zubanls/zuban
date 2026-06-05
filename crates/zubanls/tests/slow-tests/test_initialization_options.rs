11dbafe5c3e0f3f77c2ba5a51e014c556428784b
Dave Halter <davidhalter88@gmail.com>
2026-06-05 11:07:17 +0200

    Move initializition options tests

---
+++ crates/zubanls/tests/slow-tests/test_initialization_options.rs
@@ -0,0 +1,84 @@
+use lsp_types::DiagnosticServerCapabilities;
+use serde_json::json;
+use serial_test::parallel;
+
+use crate::support::Project;
+
+#[test]
+#[parallel]
+fn client_config_mode() {
+    let check = |mode: &str, has_mypy_file, is_enabled| {
+        let server = Project::with_fixture(if has_mypy_file {
+            r#"
+                [file mypy.ini]
+                [file m.py]
+                1()
+                def f():
+                    ''()
+                "#
+        } else {
+            r#"
+                [file m.py]
+                1()
+                def f():
+                    ''()
+                "#
+        })
+        .with_initialization_options(json!({ "typeCheckingMode": mode }))
+        .into_server();
+        assert_eq!(
+            is_enabled,
+            server
+                .server_capabilities
+                .as_ref()
+                .unwrap()
+                .diagnostic_provider
+                .is_some(),
+        );
+        server.diagnostics_for_file("m.py")
+    };
+
+    const NOT_INT: &str = r#""int" not callable"#;
+    const NOT_STR: &str = r#""str" not callable"#;
+
+    // Without Mypy config file
+    assert_eq!(check("default", false, true), [NOT_INT, NOT_STR]);
+    assert_eq!(check("mypy", false, true), [NOT_INT]);
+    assert_eq!(check("auto", false, true), [NOT_INT, NOT_STR]);
+    // The capability should not be announced, but if somebody requests diagnostics,it it will
+    // still work.
+    assert_eq!(check("off", false, false), [NOT_INT, NOT_STR]);
+
+    // With Mypy config file
+    assert_eq!(check("default", true, true), [NOT_INT, NOT_STR]);
+    assert_eq!(check("mypy", true, true), [NOT_INT]);
+    assert_eq!(check("auto", true, true), [NOT_INT]);
+}
+
+#[test]
+#[parallel]
+fn diagnostic_mode() {
+    let check = |mode| {
+        let server = Project::with_fixture(
+            r#"
+            [file m.py]
+            "#,
+        )
+        .with_initialization_options(json!({ "diagnosticMode": mode }))
+        .into_server();
+        let DiagnosticServerCapabilities::Options(diagnostics) = server
+            .server_capabilities
+            .as_ref()
+            .unwrap()
+            .diagnostic_provider
+            .as_ref()
+            .unwrap()
+        else {
+            unreachable!()
+        };
+        diagnostics.workspace_diagnostics
+    };
+    assert!(!check("open-files-only"));
+    assert!(!check("something-undefined"));
+    assert!(check("workspace"));
+}
