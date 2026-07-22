46d9d03d242f43bf0e699043804bcf2a4a8d4a75
Dave Halter <davidhalter88@gmail.com>
2026-07-22 17:35:42 +0200

    Recursive types should still be callable, fixes #504

--- crates/zuban_python/src/type_/operations.rs
+++ crates/zuban_python/src/type_/operations.rs
@@ -801,6 +801,10 @@ impl Type {
             Type::DataclassTransformObj(d) => {
                 Inferred::from_type(Type::DataclassTransformObj(d.clone()))
             }
+            Type::RecursiveType(t) => {
+                t.calculated_type(i_s.db)
+                    .execute(i_s, None, args, result_context, on_type_error)
+            }
             Type::Self_ => {
                 if let Some(current_type) = i_s.current_type() {
                     current_type.execute(i_s, None, args, result_context, on_type_error)
